e6950efb49ab54c9692beaa54cb26cdda3326fb6
Aron Dobos <aron.dobos@outlook.com>
2013-10-23 19:31:56 +0000

    updates to SAMnt - still buggy config dialog.....

--- src/configsel.h
+++ src/configsel.h
@@ -6,63 +6,30 @@
 #include <wx/dialog.h>
 #include <wx/event.h>
 
+#include "main.h"
+
 class wxChoiceTree;
 class wxChoiceTreeItem;
 
-
-class ConfigTree
+class ConfigDialog : public wxDialog
 {
 public:
-	ConfigTree();
-	~ConfigTree();
-	static ConfigTree &Get();
-
-	bool Load( const wxString &cfg_file );
-
-	struct TreeItem
-	{
-		~TreeItem() { Clear(); }
-		void Clear() { 
-			for( std::vector<TreeItem*>::iterator it = Children.begin();
-				it != Children.end(); ++it ) delete *it;
-			Children.clear();
-		}
-
-		wxString Label;
-		wxString Description;
-		wxString BmpName;
-		wxString TypeTag;
-		std::vector<TreeItem*> Children;
-	};
-
-	std::vector<TreeItem*> Tech() { return m_techTreeRoot.Children; }
-	std::vector<TreeItem*> Fin() { return m_finTreeRoot.Children; }
-
-private:	
-	TreeItem m_techTreeRoot;
-	TreeItem m_finTreeRoot;
-};
-
-
-
-class ConfigCtrl : public wxPanel
-{
-public:
-	ConfigCtrl(wxWindow *parent, int id, 
-		const wxPoint &pos=wxDefaultPosition, const wxSize &sz=wxDefaultSize, 
-		long flags = wxBORDER_NONE);
+	ConfigDialog( wxWindow *parent, const wxSize &size = wxSize(700,570) );
 
 	void SetConfiguration(const wxString &t, const wxString &f);
 	bool GetConfiguration(wxString &t, wxString &f);
 
+	void ShowResetCheckbox(bool b);
+	bool ResetToDefaults();
+
+private:
 	void PopulateTech();
 	bool ValidateSelections();
-private:
 	void SelectExpandByMetaTag(wxChoiceTree *aftree, const wxString &tag);
 	wxChoiceTreeItem *FindTag(wxChoiceTreeItem *parent, const wxString &tag);
-	bool CheckItemForMetaTag(ConfigTree::TreeItem *item, wxArrayString *metalist);
-	void AppendItem( wxChoiceTree *aftree, wxChoiceTreeItem *parent, ConfigTree::TreeItem *item, wxArrayString *metalist);
-	void PopulateTree( wxChoiceTree *aftree, std::vector<ConfigTree::TreeItem*> &items, wxArrayString *metalist=NULL);
+	bool CheckItemForMetaTag(ConfigDatabase::TreeItem *item, wxArrayString *metalist);
+	void AppendItem( wxChoiceTree *aftree, wxChoiceTreeItem *parent, ConfigDatabase::TreeItem *item, wxArrayString *metalist);
+	void PopulateTree( wxChoiceTree *aftree, std::vector<ConfigDatabase::TreeItem*> &items, wxArrayString *metalist=NULL);
 	void OnTechTree(wxCommandEvent &evt);
 	void OnDoubleClick(wxCommandEvent &evt);
 
@@ -71,23 +38,8 @@ private:
 
 	wxString m_t, m_f;
 
-	DECLARE_EVENT_TABLE();
-};
+	void OnHelp( wxCommandEvent &evt );
 
-class ConfigDialog : public wxDialog
-{
-public:
-	ConfigDialog( wxWindow *parent, const wxSize &size = wxSize(700,570) );
-
-	void SetConfiguration(const wxString &t, const wxString &f);
-	void GetConfiguration(wxString &t, wxString &f);
-
-	void ShowResetCheckbox(bool b);
-	bool ResetToDefaults();
-
-private:
-	void OnCommand(wxCommandEvent &evt);
-	ConfigCtrl *m_cfgCtrl;
 	wxCheckBox *m_pChkUseDefaults;
 	DECLARE_EVENT_TABLE();
 };
