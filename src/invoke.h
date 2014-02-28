ac58f090377fd7a4b59ebb2995a0454e4fb809c1
Tom Ferguson <tom.ferguson@nrel.gov>
2014-02-28 17:56:11 +0000

    Change general callback function as per Aron's request in case.cpp and invoke.cpp Renaming physical trough UI files to be consistent; change startup.lk to match new names. Adding Physical Trough Capital Costs.ui

--- src/invoke.h
+++ src/invoke.h
@@ -6,6 +6,8 @@
 lk::fcall_t* invoke_general_funcs();
 lk::fcall_t* invoke_ssc_funcs();
 lk::fcall_t* invoke_config_funcs();
+lk::fcall_t* invoke_other_funcs(); // general functions that can be called from callbacks or equations
+
 
 // these functions expect a CallbackContext* as user_data
 lk::fcall_t* invoke_uicallback_funcs();
