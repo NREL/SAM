db12424f3f46a63704318bfd50fbbf517543eb1d
Aron Dobos <aron.dobos@outlook.com>
2014-03-05 21:16:26 +0000

    added SNL inverter function in LK invoke for use in both equations and uicallbacks.

--- src/case.cpp
+++ src/case.cpp
@@ -37,7 +37,7 @@ void CaseEvaluator::SetupEnvironment( lk::env_t &env )
 	env.register_func( fcall_technology_pCase, m_case );
 	env.register_func( fcall_financing_pCase, m_case );
 	env.register_funcs( invoke_ssc_funcs() );
-	env.register_funcs( invoke_other_funcs() );
+	env.register_funcs( invoke_equation_funcs() );
 }
 	
 int CaseEvaluator::CalculateAll()
