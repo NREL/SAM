8056468013b3fbfe296cf603566c7cef276d7b0b
Steve Janzou <steven.janzou@nrel.gov>
2014-04-12 11:07:46 +0000

    Rewrite VariableGrid for case input values and case comparison - still need to update additional functionality

--- src/variablegrid.cpp
+++ src/variablegrid.cpp
@@ -1,516 +1,235 @@
-#include <vector>
 #include <algorithm>
+#include <set>
 
-#include <wx/wx.h>
-#include <wx/filename.h>
-#include <wx/dir.h>
-
-#include <ssc/sscapi.h>
 
 #include "variablegrid.h"
-#include "object.h"
-#include "main.h"
 
-class VarManager {
-public:
-	VarManager() { }
-	~VarManager() {
-		DeleteAll();
-	}
-
-	void DeleteAll() {
-		for( size_t i=0;i<m_libs.size();i++ )
-			delete m_libs[i];
-		m_libs.clear();
-	}
-
-	std::vector<VariableGrid*> m_libs;
-};
-
-static VarManager gs_libs;
-
-VariableGrid *VariableGrid::Load(std::vector<Case*> &cases)
+VariableGridData::VariableGridData(ProjectFile &project)
 {
-	VariableGrid *l = new VariableGrid;
-	if ( l->Read( cases ) )
+
+	m_cases = project.GetCases();
+	if (m_cases.size() > 0)
 	{
-		gs_libs.m_libs.push_back( l );
-		return l;
-	}
-	else
-	{
-		delete l;
-		return 0;
-	}
-}
-
-VariableGrid *VariableGrid::Find( const wxString &name )
-{
-	for( size_t i=0;i<gs_libs.m_libs.size();i++ )
-		if ( name == gs_libs.m_libs[i]->GetName() )
-			return gs_libs.m_libs[i];
-	return 0;
-}
-
-void VariableGrid::UnloadAll()
-{
-	gs_libs.DeleteAll();
-}
-
-
-VariableGrid::VariableGrid()
-{
-	m_startRow = 0;
-}
-
-bool VariableGrid::Read(std::vector<Case*> &cases)
-{
-/*	if ( !m_csv.ReadFile( file ) )
-		return false;
-
-	m_name = wxFileName(file).GetName();
-*/
-	return ScanData();
-
-}
-
-
-bool VariableGrid::ScanData()
-{
-	m_errors.clear();
-
-	m_startRow = 2;
-	/*
-	while( m_startRow < m_csv.NumRows()
-		&& !m_csv(m_startRow, 0).IsEmpty()
-		&& m_csv(m_startRow, 0)[0] == '[' )
-		m_startRow++;
-
-	if ( m_startRow >= m_csv.NumRows() )
-		return false;
-*/
-	size_t nvarlists = m_startRow-2;
-
-	if ( nvarlists < 1 )
-		return false;
-
-	m_fields.clear();
-	/*
-	size_t ncol = m_csv.NumCols();
-	for( size_t i=0;i<ncol;i++)
-	{
-		Field f;
-		if ( i == 0 ) f.Name = wxT("Name");
-		else
+		if (m_cases.size() == 1)
 		{
-			f.Name = m_csv( 0, i );
-			f.Units = m_csv( 1, i );
-		}
-
-		for( size_t j=0;j<nvarlists;j++ )
-			f.Variables.Add( m_csv( 2+j, i ) );
-
-		f.DataIndex = i;
-
-		m_fields.push_back( f );
-	}
-	*/
-	return true;
-}
-
-wxString VariableGrid::GetName() const
-{
-	return m_name;
-}
-
-wxArrayString VariableGrid::ListEntries()
-{
-	wxArrayString list;
-//	for( size_t i=m_startRow;i<m_csv.NumRows();i++ )
-//		list.Add( m_csv(i,0) );
-	return list;
-}
-
-size_t VariableGrid::NumEntries()
-{
-	return 0;// m_csv.NumRows() - m_startRow;
-}
-
-std::vector<VariableGrid::Field> &VariableGrid::GetFields()
-{
-	return m_fields;
-}
-
-int VariableGrid::GetFieldIndex( const wxString &name )
-{
-	for( size_t i=0;i<m_fields.size();i++ )
-		if ( m_fields[i].Name.Lower() == name.Lower() )
-			return i;
-
-	return -1;
-}
-
-int VariableGrid::FindEntry( const wxString &name )
-{
-//	for( size_t i=m_startRow;i<m_csv.NumRows();i++ )
-//		if ( m_csv(i,0) == name )
-//			return (int)(i-m_startRow);
-
-	return -1;
-}
-
-wxString VariableGrid::GetEntryValue( int entry, int field )
-{
-	return wxEmptyString; // m_csv(entry + m_startRow, field);
-}
-
-wxString VariableGrid::GetEntryName( int entry )
-{
-	return GetEntryValue( entry, 0 );
-}
-
-bool VariableGrid::ApplyEntry( int entry, int varindex, VarTable &tab, wxArrayString &changed )
-{
-	m_errors.Clear();
-
-	if ( varindex < 0 || varindex >= m_startRow-2 )
-	{
-		m_errors.Add( wxString::Format("invalid varindex of %d", varindex ) );
-		return false;
-	}
-
-	size_t row = m_startRow + (size_t)entry;
-/*	if ( row >= m_csv.NumRows() || row < m_startRow )
-	{
-		m_errors.Add( wxString::Format("invalid entry %d (max %d)", entry, (int)(m_csv.NumRows()-m_startRow)) );
-		return false;
-	}
-*/
-	for( size_t i=1;i<m_fields.size();i++ )
-	{
-		Field &f = m_fields[i];
-
-		wxString var;
-		if ( varindex < f.Variables.size() )
-			var = f.Variables[varindex];
-
-		if ( var.IsEmpty() ) continue; // skip this variable if no name was found
-
-		if ( VarValue *vv = tab.Get( var ) )
-		{
-//			if( VarValue::Parse( vv->Type(), m_csv(row,f.DataIndex), *vv ) )
-//				changed.Add( var );
-//			else
-				m_errors.Add( "could not parse '" + var + "' to required data type" );
+			m_col_hdrs.push_back("Variable");
+			m_col_hdrs.push_back("Label");
+			wxString case_name = project.GetCaseName(m_cases[0]);
+			m_col_hdrs.push_back(case_name);
+			m_var_table_vec.push_back(m_cases[0]->Values());
+			m_var_info_lookup_vec.push_back(m_cases[0]->Variables());
 		}
 		else
-			m_errors.Add( "variable '" + var + "' not found in collection" );
+		{
+			m_col_hdrs = project.GetCaseNames();
+			m_col_hdrs.Insert("Label", 0);
+			m_col_hdrs.Insert("Variable", 0);
+			for (std::vector<Case*>::iterator it = m_cases.begin(); it != m_cases.end(); ++it)
+			{
+				m_var_table_vec.push_back((*it)->Values());
+				m_var_info_lookup_vec.push_back((*it)->Variables());
+			}
+		}
+
+
+		m_cols = m_col_hdrs.Count();
+
+		std::set<wxString> var_names;
+		// variable names
+		for (std::vector<VarTable>::iterator it = m_var_table_vec.begin(); it != m_var_table_vec.end(); ++it)
+		{
+			wxArrayString as = it->ListAll();
+			for (size_t i = 0; i < as.Count(); i++)
+				var_names.insert(as[i]);
+		}
+
+		m_rows = var_names.size();
+
+		// variable labels
+		for (std::set<wxString>::iterator idx = var_names.begin(); idx != var_names.end(); ++idx)
+		{
+			wxString str_label = " ";
+			for (std::vector<VarInfoLookup>::iterator it = m_var_info_lookup_vec.begin(); it != m_var_info_lookup_vec.end(); ++it)
+			{
+				if ((*it).Lookup(*idx))
+					str_label = (*it).Label(*idx);
+			}
+			m_var_labels.push_back(str_label);
+			m_var_names.push_back(*idx);
+		}
+
+		/*
+		// wxGrid only support 6500 characters per cell (empirically determined) - use 1024 for display
+		size_t col = 0, row = 0;
+		int width, height;
+		std::vector<int> col_width(num_cols, 60);
+		for (std::set<wxString>::iterator idx = var_names.begin(); idx != var_names.end(); ++idx)
+		{
+			GetTextExtent(*idx, &width, &height);
+			if ((width + 10) > col_width[col]) col_width[col] = width + 10;
+			grid->SetCellValue(row, col++, *idx); //name
+			if (row < var_labels.Count())
+			{
+				GetTextExtent(var_labels[row], &width, &height);
+				if ((width + 10) > col_width[col]) col_width[col] = width + 10;
+				grid->SetCellValue(row, col++, var_labels[row]); //label
+			}
+			for (std::vector<VarTable>::iterator it = var_table_vec.begin(); it != var_table_vec.end(); ++it)
+			{
+				wxString str_val = "";
+				if (it->Get(*idx)) str_val = it->Get(*idx)->AsString();
+				if (str_val.Length() > 1024) str_val = str_val.Left(1024) + "...";
+				grid->SetCellValue(row, col++, str_val);
+			}
+			row++;
+			col = 0;
+		}
+
+		// go through all rows for case comparison and only show unequal values
+		if (m_cases.size() > 1)
+		{
+			for (row = 0; row < num_rows; row++)
+			{
+				wxString str_val = grid->GetCellValue(row, 2);
+				bool same_val = true;
+				for (col = 3; col < num_cols; col++)
+					same_val = same_val && (str_val == grid->GetCellValue(row, col));
+				if (same_val) grid->HideRow(row);
+			}
+		}
+		//grid->AutoSizeColumns();
+		// column headers
+		for (col = 0; col < col_hdrs.Count(); col++)
+		{
+			grid->SetColLabelValue(col, col_hdrs[col]);
+			GetTextExtent(col_hdrs[col], &width, &height);
+			if ((width + 10) > col_width[col]) col_width[col] = width + 10;
+			grid->SetColumnWidth(col, col_width[col]);
+		}
+		grid->Thaw();
+
+		frame->Show();
+		*/
 	}
 
-	return m_errors.Count() == 0;
 }
 
-
-VariableGridListView::VariableGridListView( VariableGridCtrl *parent, int id, const wxPoint &pos,
-	const wxSize &size )
-	: wxListView( parent, id, pos, size, wxLC_REPORT|wxLC_VIRTUAL|wxLC_SINGLE_SEL )
+int VariableGridData::GetNumberRows()
 {
-	m_variablegridctrl = parent;
+	return m_rows;
 }
 
-wxString VariableGridListView::OnGetItemText( long item, long col ) const
+int VariableGridData::GetNumberCols()
 {
-	return m_variablegridctrl->GetCellValue(item, col);
+	return m_cols;
 }
+
 /*
-wxListItemAttr *VariableGridListView::OnGetItemAttr( long item ) const
+bool VariableGridData::IsEmptyCell(int row, int col)
 {
-	static wxListItemAttr even( *wxBLACK, *wxWHITE, *wxNORMAL_FONT ),
-		odd( *wxBLACK, wxColour(245,245,245), *wxNORMAL_FONT );
-
-	return (item%2==0) ? &even : &odd;
+	if (col == 0) // variable name
+		return (m_var_names[row]==wxEmptyString);
+	else if (col == 1) // variable label
+		return (m_var_labels[row] == wxEmptyString);
+	else // get var table and value
+		return (m_var_table_vec[col].Get(m_var_names[row])->AsString() == wxEmptyString);
 }
 */
 
-enum { ID_LIST = wxID_HIGHEST+495, ID_FILTER, ID_TARGET, ID_REFRESH };
-
-BEGIN_EVENT_TABLE( VariableGridCtrl, wxPanel )
-	EVT_LIST_ITEM_SELECTED( ID_LIST, VariableGridCtrl::OnSelected )
-	EVT_LIST_COL_CLICK( ID_LIST, VariableGridCtrl::OnColClick )
-	EVT_TEXT( ID_FILTER, VariableGridCtrl::OnCommand )
-	EVT_BUTTON( ID_REFRESH, VariableGridCtrl::OnCommand )
-	EVT_CHOICE( ID_TARGET, VariableGridCtrl::OnCommand )
-END_EVENT_TABLE()
-
-
-VariableGridCtrl::VariableGridCtrl( wxWindow *parent, int id, const wxPoint &pos, const wxSize &size )
-	: wxPanel( parent, id, pos, size, wxTAB_TRAVERSAL )
+wxString VariableGridData::GetColLabelValue(int col)
 {
-	m_sendEvents = true;
-
-	m_label = new wxStaticText( this, wxID_ANY, wxT("Search for:") );
-	m_filter = new wxTextCtrl( this, ID_FILTER );
-	m_target = new wxChoice( this, ID_TARGET );
-	m_notify = new wxStaticText( this, wxID_ANY, wxEmptyString );
-	m_notify->SetForegroundColour( *wxRED );
-	m_list = new VariableGridListView( this, ID_LIST );
-
-	wxBoxSizer *sz_horiz = new wxBoxSizer( wxHORIZONTAL );
-	sz_horiz->Add( m_label, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5 );
-	sz_horiz->Add( m_filter, 0, wxALL|wxALIGN_CENTER_VERTICAL, 3 );
-	sz_horiz->Add( m_target, 0, wxALL|wxALIGN_CENTER_VERTICAL, 3 );
-	sz_horiz->AddStretchSpacer();
-	sz_horiz->Add( m_notify, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5 );
-	//sz_horiz->Add( new wxButton( this, ID_REFRESH, wxT("Refresh list") ), 0, wxALL|wxALIGN_CENTER_VERTICAL, 3 );
-
-	wxBoxSizer *sz_vert = new wxBoxSizer( wxVERTICAL );
-	sz_vert->Add( sz_horiz, 0, wxALL|wxEXPAND, 0 );
-	sz_vert->Add( m_list, 1, wxALL|wxEXPAND, 0 );
-	SetSizer( sz_vert );
+	return m_col_hdrs[col];
 }
 
-VariableGridCtrl::~VariableGridCtrl()
+wxString VariableGridData::GetValue(int row, int col)
 {
-	// nothing to do
+	if (col == 0) // variable name
+		return m_var_names[row];
+	else if (col == 1) // variable label
+		return m_var_labels[row];
+	else // get var table and value
+		return m_var_table_vec[col-2].Get(m_var_names[row])->AsString();
 }
 
-bool VariableGridCtrl::SetEntrySelection( const wxString &entry )
+void VariableGridData::SetValue(int row, int col, const wxString& value)
+{ // must update labels and values as necessary
+	// TODO
+}
+
+
+VariableGridFrame::VariableGridFrame(wxWindow *parent, ProjectFile &project) : wxFrame(parent, wxID_ANY, "Variable Grid", wxDefaultPosition, wxSize(400, 700))
 {
-	if ( VariableGrid *lib = VariableGrid::Find( m_variablegrid ) )
+
+	if (project.GetCases().size() > 0)
 	{
-		int entry_idx = lib->FindEntry( entry );
-		if ( entry_idx >= 0 )
+		wxString title;
+		if (project.GetCases().size() == 1)
 		{
-			int to_sel = -1;
-			// find in current view
-
-			for( size_t i=0;i<m_view.size();i++ )
-			{
-				if ( m_view[i].index == entry_idx )
-				{
-					to_sel = i;
-					break;
-				}
-			}
-
-			if ( to_sel < 0 )
-			{
-				// selecting item that is not in the currently filtered view,
-				// so we must add it to the view
-				m_view.push_back( viewable(entry, entry_idx) );
-				m_list->SetItemCount( m_view.size() );
-				m_list->Refresh();
-
-				to_sel = m_view.size()-1;
-			}
-
-			m_list->SetItemState( to_sel, wxLIST_STATE_SELECTED|wxLIST_STATE_FOCUSED,
-				wxLIST_STATE_SELECTED|wxLIST_STATE_FOCUSED );
-			m_list->EnsureVisible( to_sel );
-
-			return true;
-		}
-	}
-
-	return false;
-}
-
-wxString VariableGridCtrl::GetEntrySelection()
-{
-	long sel = m_list->GetFirstSelected();
-	if ( sel >= 0 && sel < m_view.size() ) return m_view[ sel ].name;
-	return wxEmptyString;
-}
-
-wxString VariableGridCtrl::GetCellValue( long item, long col )
-{
-	if ( VariableGrid *lib = VariableGrid::Find( m_variablegrid ) )
-		if ( item < m_view.size() && col < m_fieldMap.size() )
-			return lib->GetEntryValue( m_view[item].index, m_fieldMap[col] );
-
-	return wxT("<inval>");
-}
-
-void VariableGridCtrl::SetVariableGrid( const wxString &name, const wxString &fields )
-{
-	m_variablegrid = name;
-
-	if( VariableGrid *lib = VariableGrid::Find( m_variablegrid ) )
-	{
-		m_fields.Clear();
-		m_fieldMap.clear();
-
-		if ( fields == "*" )
-		{
-			std::vector<VariableGrid::Field> &ff = lib->GetFields();
-			for( size_t i=0;i<ff.size();i++ )
-			{
-				m_fields.Add( ff[i].Name );
-				m_fieldMap.push_back( i );
-			}
+			wxString case_name = project.GetCaseName(project.GetCases()[0]);
+			title = "Current Case Values: " + case_name;
 		}
 		else
 		{
-			wxArrayString fnames = wxSplit( fields, ',' );
-			for( size_t i=0;i<fnames.size();i++ )
+			title = "Case comparison";
+		}
+		
+		SetTitle(title);
+		m_griddata = new VariableGridData(project);
+
+		m_grid = new wxGrid(this, wxID_ANY);
+		m_grid->Freeze();
+		m_grid->SetTable(m_griddata, true, wxGrid::wxGridSelectRows);
+		/*
+		// wxGrid only support 6500 characters per cell (empirically determined) - use 1024 for display
+		size_t col = 0, row = 0;
+		int width, height;
+		std::vector<int> col_width(num_cols, 60);
+		for (std::set<wxString>::iterator idx = var_names.begin(); idx != var_names.end(); ++idx)
+		{
+			GetTextExtent(*idx, &width, &height);
+			if ((width + 10) > col_width[col]) col_width[col] = width + 10;
+			grid->SetCellValue(row, col++, *idx); //name
+			if (row < var_labels.Count())
 			{
-				int idx = lib->GetFieldIndex( fnames[i] );
-				if ( idx >= 0 )
-				{
-					m_fields.Add( fnames[i] );
-					m_fieldMap.push_back( lib->GetFieldIndex( fnames[i] ) );
-				}
+				GetTextExtent(var_labels[row], &width, &height);
+				if ((width + 10) > col_width[col]) col_width[col] = width + 10;
+				grid->SetCellValue(row, col++, var_labels[row]); //label
+			}
+			for (std::vector<VarTable>::iterator it = var_table_vec.begin(); it != var_table_vec.end(); ++it)
+			{
+				wxString str_val = "";
+				if (it->Get(*idx)) str_val = it->Get(*idx)->AsString();
+				if (str_val.Length() > 1024) str_val = str_val.Left(1024) + "...";
+				grid->SetCellValue(row, col++, str_val);
+			}
+			row++;
+			col = 0;
+		}
+
+		// go through all rows for case comparison and only show unequal values
+		if (m_cases.size() > 1)
+		{
+			for (row = 0; row < num_rows; row++)
+			{
+				wxString str_val = grid->GetCellValue(row, 2);
+				bool same_val = true;
+				for (col = 3; col < num_cols; col++)
+					same_val = same_val && (str_val == grid->GetCellValue(row, col));
+				if (same_val) grid->HideRow(row);
 			}
 		}
-
-		m_sortDir.resize( m_fieldMap.size(), true );
-	}
-
-	ReloadVariableGrid();
-
-}
-
-void VariableGridCtrl::ReloadVariableGrid()
-{
-	if( VariableGrid *lib = VariableGrid::Find( m_variablegrid ) )
-	{
-		wxString tarsel = m_target->GetStringSelection();
-		m_target->Clear();
-
-		m_list->ClearAll();
-		for( size_t i=0;i<m_fields.size();i++ )
+		//grid->AutoSizeColumns();
+		// column headers
+		for (col = 0; col < col_hdrs.Count(); col++)
 		{
-			m_list->AppendColumn( m_fields[i] );
-			m_target->Append( m_fields[i] );
+			grid->SetColLabelValue(col, col_hdrs[col]);
+			GetTextExtent(col_hdrs[col], &width, &height);
+			if ((width + 10) > col_width[col]) col_width[col] = width + 10;
+			grid->SetColumnWidth(col, col_width[col]);
 		}
+		*/
+		m_grid->Thaw();
 
-		if ( tarsel.IsEmpty()  && m_target->GetCount() > 0 )
-			m_target->SetSelection( 0 );
-		else
-			m_target->SetStringSelection( tarsel );
-
-		m_entries = lib->ListEntries();
-
-		UpdateList();
-
-		m_list->SetColumnWidth( 0, 350 );
-
+		Show();
 	}
-	else
-		wxLogStatus( "VariableGridCtrl: could not find variablegrid " + m_variablegrid );
 }
-
-void VariableGridCtrl::UpdateList()
-{
-	m_sendEvents = false;
-
-	wxString filter = m_filter->GetValue().Lower();
-	wxString sel = GetEntrySelection();
-
-	m_notify->SetLabel( wxEmptyString );
-	m_view.clear();
-	if ( m_entries.size() > 0 )
-		m_view.reserve( m_entries.size() );
-
-	size_t nmatches = 0;
-
-	if( VariableGrid *lib = VariableGrid::Find( m_variablegrid ) )
-	{
-		size_t num_entries = lib->NumEntries();
-
-		size_t target_field_idx = 0;
-		int t_sel = m_target->GetSelection();
-		if ( t_sel >= 0 && t_sel < m_fieldMap.size() )
-			target_field_idx = m_fieldMap[t_sel];
-
-		for (size_t i=0;i<num_entries;i++)
-		{
-			wxString target = lib->GetEntryValue( i, target_field_idx );
-
-			if ( filter.IsEmpty()
-				|| (filter.Len() <= 2 && target.Left( filter.Len() ).Lower() == filter)
-				|| (target.Lower().Find( filter ) >= 0)
-				)
-			{
-				m_view.push_back( viewable(m_entries[i], i) );
-				nmatches++;
-			}
-			else if ( target == sel )
-				m_view.push_back( viewable(m_entries[i], i) );
-		}
-	}
-
-	if ( nmatches == 0 )
-	{
-		m_notify->SetLabel( "No matches. (selected item shown)" );
-		Layout();
-	}
-
-	m_list->SetItemCount( m_view.size() );
-	m_list->Refresh();
-	SetEntrySelection( sel );
-
-	m_sendEvents = true;
-}
-
-void VariableGridCtrl::OnSelected( wxListEvent &evt )
-{
-	if ( !m_sendEvents ) return;
-
-	wxCommandEvent issue( wxEVT_LISTBOX, GetId() );
-	issue.SetEventObject( this );
-	issue.SetInt( evt.GetSelection() );
-	ProcessEvent( issue );
-
-}
-
-bool VariableGridCtrl::viewable_compare::operator() ( const viewable &lhs, const viewable &rhs )
-{
-	if ( dir && col[lhs.index] < col[rhs.index] ) return true;
-	else if ( !dir && col[lhs.index] > col[rhs.index] ) return true;
-	else return false;
-}
-
-void VariableGridCtrl::OnColClick( wxListEvent &evt )
-{
-	wxBusyCursor wait;
-
-	if( VariableGrid *lib = VariableGrid::Find( m_variablegrid ) )
-	{
-		m_sendEvents = false;
-
-		wxString sel = GetEntrySelection();
-
-		size_t field_idx = m_fieldMap[evt.GetColumn()];
-
-		wxArrayString column;
-		column.reserve( m_entries.size() );
-		for( size_t i=0;i<m_entries.size();i++ )
-			column.push_back( lib->GetEntryValue( i, field_idx ) );
-
-		viewable_compare cc( column, ! m_sortDir[evt.GetColumn()] );
-		std::stable_sort( m_view.begin(), m_view.end(), cc );
-
-		m_sortDir[ evt.GetColumn() ] = ! m_sortDir[evt.GetColumn()];
-
-		m_list->Refresh();
-		SetEntrySelection( sel );
-
-		m_sendEvents = true;
-	}
-
-}
-
-void VariableGridCtrl::OnCommand( wxCommandEvent &evt )
-{
-	if( evt.GetId() == ID_FILTER || evt.GetId() == ID_TARGET )
-		UpdateList();
-}
-
-void VariableGridCtrl::SetLabel( const wxString &text )
-{
-	m_label->SetLabel( text );
-	Layout();
-}
-
-
--- src/variablegrid.h
+++ src/variablegrid.h
@@ -2,139 +2,45 @@
 #define __variablegrid_h
 
 #include <wx/string.h>
-#include <wx/listctrl.h>
-#include <wx/panel.h>
+#include <wx/grid.h>
+#include <wx/frame.h>
 
 #include "variables.h"
+#include "project.h"
 #include "case.h"
 
+#include <vector>
 
-/* Example variablegrid.
-*/
 
-class VariableGrid
+
+class VariableGridData : public wxGridTableBase
 {
 public:
-	VariableGrid();
-
-	static VariableGrid *Load(std::vector<Case*> &cases);
-	static void UnloadAll();
-	static VariableGrid *Find( const wxString &name );
-
-	struct Field
-	{
-		wxString Name;
-		wxString Units;
-		wxArrayString Variables;
-		int DataIndex;
-	};
-
-	bool Read( std::vector<Case*> &cases );
-	wxString GetName() const;
-	wxArrayString &GetErrors() { return m_errors; }
-
-	wxArrayString ListEntries();
-	size_t NumEntries();
-	std::vector<Field> &GetFields();
-	int GetFieldIndex( const wxString &name );
-	int FindEntry( const wxString &name );
-	wxString GetEntryValue( int entry, int field );
-	wxString GetEntryName( int entry );
-	bool ApplyEntry( int entry, int varindex, VarTable &tab, wxArrayString &changed );
-
+	VariableGridData(ProjectFile &project);
+	virtual int GetNumberRows();
+	virtual int GetNumberCols();
+//	virtual bool IsEmptyCell(int row, int col);
+	virtual wxString GetValue(int row, int col);
+	virtual void SetValue(int row, int col, const wxString& value);
+	virtual wxString GetColLabelValue(int col);
 private:
-
-	bool ScanData();
-
-	wxString m_name;
-	std::vector<Field> m_fields;
-	size_t m_startRow;
-	wxArrayString m_errors;
+	int m_rows;
+	int m_cols;
+	std::vector<Case*> m_cases;
+	wxArrayString m_col_hdrs;
+	wxArrayString m_var_names;
+	wxArrayString m_var_labels;
+	std::vector<VarTable> m_var_table_vec;
+	std::vector<VarInfoLookup> m_var_info_lookup_vec;
 };
 
-
-class VariableGridCtrl;
-class wxTextCtrl;
-class wxStaticText;
-class wxChoice;
-
-class VariableGridListView : public wxListView
-{
-	VariableGridCtrl *m_variablegridctrl;
-public:
-	VariableGridListView( VariableGridCtrl *parent, int id, const wxPoint &pos = wxDefaultPosition,
-		const wxSize &size = wxDefaultSize );
-
-	virtual wxString OnGetItemText( long item, long col ) const;
-};
-
-#define EVT_VARIABLEGRIDCTRL(id, func) EVT_LISTBOX(id,func)
-
-class VariableGridCtrl : public wxPanel
+class VariableGridFrame : public wxFrame
 {
 public:
-	VariableGridCtrl( wxWindow *parent, int id, const wxPoint &pos = wxDefaultPosition,
-		const wxSize &size = wxDefaultSize );
-	virtual ~VariableGridCtrl();
-
-	void SetLabel( const wxString &label );
-
-	void SetVariableGrid( const wxString &name, const wxString &fields );
-	void ReloadVariableGrid();
-
-	bool SetEntrySelection( const wxString &entry );
-	wxString GetEntrySelection();
-
-
-	wxString GetCellValue( long item, long col );
-	void UpdateList();
-
-protected:
-	void OnSelected( wxListEvent & );
-	void OnColClick( wxListEvent & );
-	void OnCommand( wxCommandEvent & );
-
+	VariableGridFrame(wxWindow *parent, ProjectFile &project);
 private:
-	wxString m_variablegrid;
-	bool m_inclEntryName;
-
-	std::vector<bool> m_sortDir;
-
-	wxArrayString m_fields;
-	std::vector<size_t> m_fieldMap;
-
-	wxArrayString m_entries;
-
-	struct viewable
-	{
-		viewable( const wxString &n, size_t i ) : name(n), index(i) { }
-		wxString name;
-		size_t index;
-	};
-
-	class viewable_compare
-	{
-		wxArrayString &col;
-		bool dir;
-	public:
-		viewable_compare( wxArrayString &c,  bool d ) : col(c), dir(d) { }
-		bool operator() ( const viewable &lhs, const viewable &rhs );
-	};
-
-	std::vector<viewable> m_view;
-
-	wxStaticText *m_label;
-	wxTextCtrl *m_filter;
-	wxStaticText *m_notify;
-	wxChoice *m_target;
-	VariableGridListView *m_list;
-
-	bool m_sendEvents;
-
-
-	DECLARE_EVENT_TABLE();
+	wxGrid *m_grid;
+	VariableGridData *m_griddata;
 };
-
-
 
 #endif
