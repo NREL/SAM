a7b81005c57b187773b49be7cb5d23a6afc1cc7f
Aron Dobos <aron.dobos@outlook.com>
2014-05-15 21:16:14 +0000

    added outlines for parametric window and data structure

--- src/casewin.cpp
+++ src/casewin.cpp
@@ -24,6 +24,7 @@
 #include "casewin.h"
 #include "ipagelist.h"
 #include "simulation.h"
+#include "parametric.h"
 
 #include "../resource/graph.cpng"
 
@@ -184,69 +185,11 @@ CaseWindow::CaseWindow( wxWindow *parent, Case *c )
 	
 	m_baseCaseResults = new ResultsViewer( m_pageFlipper, ID_BASECASE_PAGES );
 	m_pageFlipper->AddPage( m_baseCaseResults, "Base Case" );
+		
+	m_parametrics = new ParametricViewer( m_pageFlipper, m_case );
+	m_pageFlipper->AddPage( m_parametrics, "Parametric", false );
 
-	wxPanel *param_panel = new wxPanel( m_pageFlipper );
 
-	wxBoxSizer *par_sizer = new wxBoxSizer( wxHORIZONTAL );
-	par_sizer->Add( new wxButton( param_panel, wxID_ANY, "Configure..."), 0, wxALL|wxEXPAND, 2 );
-	par_sizer->Add( new wxButton( param_panel, wxID_ANY, "Run parametric simulation"), 0, wxALL|wxEXPAND, 2 );
-	par_sizer->AddStretchSpacer();
-	par_sizer->Add( new wxButton( param_panel, wxID_ANY, "Clear results"), 0, wxALL|wxEXPAND, 2 );
-	
-	wxExtGridCtrl *par_grid = new wxExtGridCtrl( param_panel, wxID_ANY );
-	par_grid->CreateGrid( 7, 4 );
-
-	wxString grid[8][5]= {
-		{ "Tilt", "Input 2", "LCOEreal", "AnnOutput", "NPV" },
-		{ "10", "1", "1",   "1", "-4" },
-		{ "15", "2", "3.1", "2", "-3" },
-		{ "20", "3", "4.1", "3", "-2" },
-		{ "25", "4", "3.1", "4", "-1" },
-		{ "30", "5", "2.1", "5", "-2" },
-		{ "35", "6", "1.1", "6", "-3" },
-		{ "40", "7", "1.1", "7", "-4" } };
-
-	for( size_t r=1;r<8;r++ )
-	{
-		for( size_t c=1;c<5;c++ )
-		{
-			if ( r == 1 ) par_grid->SetColLabelValue( c-1, grid[0][c-1] );
-			par_grid->SetCellValue( grid[r][c], r-1, c-1 );
-			if ( c <= 2 ) par_grid->SetCellBackgroundColour( wxColour(244,244,210), r-1, c-1 );
-		}
-	}
-	par_grid->AutoSizeColumns();
-
-	wxPLPlotCtrl *par_plot = new wxPLPlotCtrl( param_panel, wxID_ANY );
-	std::vector<wxRealPoint> bar_data;
-	bar_data.push_back( wxRealPoint( 1, 3 ) );
-	bar_data.push_back( wxRealPoint( 2, 2.9 ) );
-	bar_data.push_back( wxRealPoint( 3, 3.2 ) );
-	bar_data.push_back( wxRealPoint( 4, 3.7 ) );
-	bar_data.push_back( wxRealPoint( 5, 2.2 ) );
-	bar_data.push_back( wxRealPoint( 6, 1.7 ) );
-	wxPLBarPlot *bar0, *bar1;
-	par_plot->AddPlot( bar0 = new wxPLBarPlot( bar_data, "Var2 run^0", wxMetroTheme::Colour( wxMT_ACCENT ) ) );
-	for( size_t i=0;i<bar_data.size();i++ ) bar_data[i].y *= 0.7 + ((double)i)/5;
-	par_plot->AddPlot( bar1 = new wxPLBarPlot( bar_data, "Var1 run^0", wxMetroTheme::Colour( wxMT_FOREGROUND ) ) );
-	par_plot->GetXAxis1()->SetWorld( 0, 7 );
-	par_plot->GetYAxis1()->SetWorld( 0, 5 );
-	std::vector<wxPLBarPlot*> bar_group;
-	bar_group.push_back( bar0 );
-	bar_group.push_back( bar1 );
-	bar0->SetGroup( bar_group );
-	bar1->SetGroup( bar_group );
-	bar0->SetThickness( 30 );
-	bar1->SetThickness( 30 );
-
-	wxBoxSizer *par_vsizer = new wxBoxSizer( wxVERTICAL );
-	par_vsizer->Add( par_sizer, 0, wxALL|wxEXPAND, 2 );
-	par_vsizer->Add( par_grid, 1, wxALL|wxEXPAND, 0 );
-	par_vsizer->Add( par_plot, 1, wxALL|wxEXPAND, 0 );
-
-	param_panel->SetSizer( par_vsizer );
-
-	m_pageFlipper->AddPage( param_panel, "Parametric", false );
 	m_pageFlipper->AddPage( new wxPanel( m_pageFlipper ), "Sensitivity", false );
 	m_pageFlipper->AddPage( new wxPanel( m_pageFlipper ), "P50/P90", false );
 
