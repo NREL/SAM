bc8810eed6d34c9f6bd410a782b4480cc46b91e4
Aron Dobos <aron.dobos@outlook.com>
2014-06-02 18:29:39 +0000

    Added outlines of help browser.

---
+++ deploy/runtime/help/html/samhelp_contextids.js
@@ -0,0 +1,37 @@
+var hmContextIds = new Array();
+function hmGetContextId(query) {
+    var urlParams;
+    var match,
+        pl = /\+/g,
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+    params = {};
+    while (match = search.exec(query))
+       params[decode(match[1])] = decode(match[2]);
+    if (params["contextid"]) return decodeURIComponent(hmContextIds[params["contextid"]]);
+    else return "";
+}
+
+hmContextIds["41400"]="pv_shading.htm";
+hmContextIds["41500"]="pvfp_module.htm";
+hmContextIds["41600"]="pvfp_inverter.htm";
+hmContextIds["41200"]="pvfp_array.htm";
+hmContextIds["42000"]="parabolic_trough_empirical.htm";
+hmContextIds["42200"]="troughempirical_solar_field.htm";
+hmContextIds["42300"]="troughempirical_sca_hce.htm";
+hmContextIds["42400"]="troughempirical_power_block.htm";
+hmContextIds["42500"]="troughempirical_thermal_storage.htm";
+hmContextIds["42600"]="troughempirical_parasitics.htm";
+hmContextIds["44000"]="power_tower_molten_salt.htm";
+hmContextIds["44300"]="towerms_heliostat_field.htm";
+hmContextIds["44400"]="towerms_tower_and_receiver.htm";
+hmContextIds["44500"]="towerms_power_cycle.htm";
+hmContextIds["44600"]="towerms_thermal_storage.htm";
+hmContextIds["44700"]="towerms_parasitics.htm";
+hmContextIds["43000"]="dish_stirling.htm";
+hmContextIds["43000"]="dish_stirling.htm";
+hmContextIds["43000"]="dish_stirling.htm";
+hmContextIds["43000"]="dish_stirling.htm";
+hmContextIds["43000"]="dish_stirling.htm";
+hmContextIds["45200"]="generic_system_plant.htm";
+hmContextIds["300000"]="references.htm";
