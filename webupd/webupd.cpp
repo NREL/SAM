215a1f66cefca87497fadd7c4aaa2ae63549c92e
Steve Janzou <steven.janzou@nrel.gov>
2015-01-20 11:30:06 +0000

    Update version number for pending 2015.1.21 internal release

--- webupd/webupd.cpp
+++ webupd/webupd.cpp
@@ -32,7 +32,7 @@
 #include "pkgdlg.h"
 
 static wxString g_updateURL("https://sam.nrel.gov/sites/sam.nrel.gov/files/content/updates/");
-static wxString g_samVerIdStr("2015.1.16");
+static wxString g_samVerIdStr("2015.1.21");
 static wxString g_appPath;
 static wxString g_icmStr; // internet connection method string
 
