317f026cdfdef0f0ef81a436656be7c523a89e8b
Paul Gilman <paulgilman@earthlink.net>
2016-02-01 21:19:58 +0000

    update build batch scripts

--- build_vc2013/build.bat
+++ build_vc2013/build.bat
@@ -17,15 +17,15 @@ TortoiseProc.exe /command:update /path:%SAMNTDIR% /closeonend:1
 cd\
 cd C:\Program Files (x86)\Microsoft Visual Studio 12.0
 
-msbuild %LKDIR%\vc2013_wx3\lkvc13wx3.sln /p:Configuration=Debug;Platform=win32
-msbuild %LKDIR%\vc2013_wx3\lkvc13wx3.sln /p:Configuration=Release;Platform=win32
-msbuild %LKDIR%\vc2013_wx3\lkvc13wx3.sln /p:Configuration=Debug;Platform=x64
-msbuild %LKDIR%\vc2013_wx3\lkvc13wx3.sln /p:Configuration=Release;Platform=x64
+msbuild %LKDIR%\build_vc2013\lkvc13wx3.sln /p:Configuration=Debug;Platform=win32
+msbuild %LKDIR%\build_vc2013\lkvc13wx3.sln /p:Configuration=Release;Platform=win32
+msbuild %LKDIR%\build_vc2013\lkvc13wx3.sln /p:Configuration=Debug;Platform=x64
+msbuild %LKDIR%\build_vc2013\lkvc13wx3.sln /p:Configuration=Release;Platform=x64
 
-msbuild %WEXDIR%\vc2013_wx3\wexvc13wx3.sln /p:Configuration=Debug;Platform=win32
-msbuild %WEXDIR%\vc2013_wx3\wexvc13wx3.sln /p:Configuration=Release;Platform=win32
-msbuild %WEXDIR%\vc2013_wx3\wexvc13wx3.sln /p:Configuration=Debug;Platform=x64
-msbuild %WEXDIR%\vc2013_wx3\wexvc13wx3.sln /p:Configuration=Release;Platform=x64
+msbuild %WEXDIR%\build_vc2013\wexvc13wx3.sln /p:Configuration=Debug;Platform=win32
+msbuild %WEXDIR%\build_vc2013\wexvc13wx3.sln /p:Configuration=Release;Platform=win32
+msbuild %WEXDIR%\build_vc2013\wexvc13wx3.sln /p:Configuration=Debug;Platform=x64
+msbuild %WEXDIR%\build_vc2013\wexvc13wx3.sln /p:Configuration=Release;Platform=x64
 
 msbuild %SSCDIR%\build_vc2013\ssc_vc2013.sln /p:Configuration=Debug;Platform=win32
 msbuild %SSCDIR%\build_vc2013\ssc_vc2013.sln /p:Configuration=Release;Platform=win32
@@ -35,9 +35,9 @@ msbuild %SSCDIR%\build_vc2013\ssc_vc2013.sln /p:Configuration=Release;Platform=x
 copy %SSCDIR%\build_vc2013\Win32\Release\ssc.dll %SAMNTDIR%\deploy\win32
 copy %SSCDIR%\build_vc2013\x64\Release\ssc.dll %SAMNTDIR%\deploy\x64
 
-msbuild %SAMNTDIR%\vc2013_wx3\SAMnt_vc2013.sln /p:Configuration=Debug;Platform=win32
-msbuild %SAMNTDIR%\vc2013_wx3\SAMnt_vc2013.sln /p:Configuration=Release;Platform=win32
-msbuild %SAMNTDIR%\vc2013_wx3\SAMnt_vc2013.sln /p:Configuration=Debug;Platform=x64
-msbuild %SAMNTDIR%\vc2013_wx3\SAMnt_vc2013.sln /p:Configuration=Release;Platform=x64
+msbuild %SAMNTDIR%\build_vc2013\SAMnt_vc2013.sln /p:Configuration=Debug;Platform=win32
+msbuild %SAMNTDIR%\build_vc2013\SAMnt_vc2013.sln /p:Configuration=Release;Platform=win32
+msbuild %SAMNTDIR%\build_vc2013\SAMnt_vc2013.sln /p:Configuration=Debug;Platform=x64
+msbuild %SAMNTDIR%\build_vc2013\SAMnt_vc2013.sln /p:Configuration=Release;Platform=x64
 
 cd\
