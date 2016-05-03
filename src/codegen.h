5964cf10583ce950340e5a32d51cdb62e01b396d
Steve Janzou <steven.janzou@nrel.gov>
2016-05-03 13:19:21 +0000

    Code generator update - initial working lk and c versions Add CEC inverter coefficient generator workbook to References/SandiaInvModCoeff_CEC_3.4 2015.xlsm

--- src/codegen.h
+++ src/codegen.h
@@ -36,22 +36,22 @@ public:
 
 	void Clear();
 	Case *GetCase() { return m_case; }
+	wxString GetErrors();
 
-	
 	// language specfic header and supporting functions
 	virtual bool Header(FILE *fp)=0;
-	virtual bool CreateSSCData(FILE *fp, wxString &name) = 0;
-	virtual bool FreeSSCData(FILE *fp, wxString &name) = 0;
+
 	virtual bool CreateSSCModule(FILE *fp, wxString &name) = 0;
 	virtual bool RunSSCModule(FILE *fp, wxString &name) = 0;
-	virtual bool FreeSSCModule(FILE *fp, wxString &name) = 0;
-//	virtual bool SetSSCVariable(FILE *fp)=0;
-//	virtual bool GetSSCVariable(FILE *fp)=0;
+	virtual bool FreeSSCModule(FILE *fp) = 0;
+
 	virtual bool Input(FILE *fp, ssc_data_t p_data, const char *name, const wxString &folder, const int &array_matrix_threshold) = 0;
-	// language specific output specification (e.g. printf)
-	virtual bool Output(FILE *fp)=0;
-	
-	// try to make same across languages
+	virtual bool Output(FILE *fp, ssc_data_t p_data) = 0;
+
+	virtual bool Footer(FILE *fp) = 0;
+
+
+	// same across languages
 	bool GenerateCode(FILE *fp, const int &array_matrix_threshold);
 	bool Prepare();
 	bool Ok();
@@ -71,16 +71,25 @@ class CodeGen_lk : virtual public CodeGen_Base
 public:
 	CodeGen_lk(Case *cc, const wxString &folder);
 	bool Header(FILE *fp);
-	bool CreateSSCData(FILE *fp, wxString &name);
-	bool FreeSSCData(FILE *fp, wxString &name);
 	bool CreateSSCModule(FILE *fp, wxString &name);
 	bool RunSSCModule(FILE *fp, wxString &name);
-	bool FreeSSCModule(FILE *fp, wxString &name);
-//	bool SetSSCVariable(FILE *fp);
-//	bool GetSSCVariable(FILE *fp);
+	bool FreeSSCModule(FILE *fp);
 	bool Input(FILE *fp, ssc_data_t p_data, const char *name, const wxString &folder, const int &array_matrix_threshold);
-	// language specific output specification (e.g. printf)
-	bool Output(FILE *fp);
-
+	bool Output(FILE *fp, ssc_data_t p_data);
+	bool Footer(FILE *fp);
 };
+
+class CodeGen_c : virtual public CodeGen_Base
+{
+public:
+	CodeGen_c(Case *cc, const wxString &folder);
+	bool Header(FILE *fp);
+	bool CreateSSCModule(FILE *fp, wxString &name);
+	bool RunSSCModule(FILE *fp, wxString &name);
+	bool FreeSSCModule(FILE *fp);
+	bool Input(FILE *fp, ssc_data_t p_data, const char *name, const wxString &folder, const int &array_matrix_threshold);
+	bool Output(FILE *fp, ssc_data_t p_data);
+	bool Footer(FILE *fp);
+};
+
 #endif
