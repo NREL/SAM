cb26dab3394904731f24897b3fad747af17eb8ac
Steve Janzou <steven.janzou@nrel.gov>
2016-05-18 13:10:51 +0000

    code generator updates - lk, c, csharp and matlab working.

--- src/codegen.h
+++ src/codegen.h
@@ -32,27 +32,26 @@ struct CodeGenData {
 class CodeGen_Base
 {
 public:
-	CodeGen_Base( Case *cc, const wxString &folder );
-
+	CodeGen_Base( Case *cc, const wxString &fullpath );
+	~CodeGen_Base();
 	void Clear();
 	Case *GetCase() { return m_case; }
 	wxString GetErrors();
 
 	// language specfic header and supporting functions
-	virtual bool Header(FILE *fp)=0;
+	virtual bool Header()=0;
 
-	virtual bool CreateSSCModule(FILE *fp, wxString &name) = 0;
-	virtual bool RunSSCModule(FILE *fp, wxString &name) = 0;
-	virtual bool FreeSSCModule(FILE *fp) = 0;
+	virtual bool CreateSSCModule(wxString &name) = 0;
+	virtual bool RunSSCModule(wxString &name) = 0;
+	virtual bool FreeSSCModule() = 0;
 
-	virtual bool Input(FILE *fp, ssc_data_t p_data, const char *name, const wxString &folder, const int &array_matrix_threshold) = 0;
-	virtual bool Output(FILE *fp, ssc_data_t p_data) = 0;
+	virtual bool Input(ssc_data_t p_data, const char *name, const wxString &folder, const int &array_matrix_threshold) = 0;
+	virtual bool Output(ssc_data_t p_data) = 0;
 
-	virtual bool Footer(FILE *fp) = 0;
-
+	virtual bool Footer() = 0;
 
 	// same across languages
-	bool GenerateCode(FILE *fp, const int &array_matrix_threshold);
+	bool GenerateCode(const int &array_matrix_threshold);
 	bool Prepare();
 	bool Ok();
 	void AddData(CodeGenData md);
@@ -60,7 +59,9 @@ public:
 
 protected:
 	Case *m_case;
+	FILE *m_fp;
 	wxString m_folder;
+	wxString m_name;
 	wxArrayString m_errors;
 	VarTable m_inputs;
 	std::vector<CodeGenData> m_data;
@@ -70,52 +71,52 @@ class CodeGen_lk : virtual public CodeGen_Base
 {
 public:
 	CodeGen_lk(Case *cc, const wxString &folder);
-	bool Header(FILE *fp);
-	bool CreateSSCModule(FILE *fp, wxString &name);
-	bool RunSSCModule(FILE *fp, wxString &name);
-	bool FreeSSCModule(FILE *fp);
-	bool Input(FILE *fp, ssc_data_t p_data, const char *name, const wxString &folder, const int &array_matrix_threshold);
-	bool Output(FILE *fp, ssc_data_t p_data);
-	bool Footer(FILE *fp);
+	bool Header();
+	bool CreateSSCModule(wxString &name);
+	bool RunSSCModule(wxString &name);
+	bool FreeSSCModule();
+	bool Input(ssc_data_t p_data, const char *name, const wxString &folder, const int &array_matrix_threshold);
+	bool Output(ssc_data_t p_data);
+	bool Footer();
 };
 
 class CodeGen_c : virtual public CodeGen_Base
 {
 public:
 	CodeGen_c(Case *cc, const wxString &folder);
-	bool Header(FILE *fp);
-	bool CreateSSCModule(FILE *fp, wxString &name);
-	bool RunSSCModule(FILE *fp, wxString &name);
-	bool FreeSSCModule(FILE *fp);
-	bool Input(FILE *fp, ssc_data_t p_data, const char *name, const wxString &folder, const int &array_matrix_threshold);
-	bool Output(FILE *fp, ssc_data_t p_data);
-	bool Footer(FILE *fp);
+	bool Header();
+	bool CreateSSCModule(wxString &name);
+	bool RunSSCModule(wxString &name);
+	bool FreeSSCModule();
+	bool Input(ssc_data_t p_data, const char *name, const wxString &folder, const int &array_matrix_threshold);
+	bool Output(ssc_data_t p_data);
+	bool Footer();
 };
 
 class CodeGen_csharp : virtual public CodeGen_Base
 {
 public:
 	CodeGen_csharp(Case *cc, const wxString &folder);
-	bool Header(FILE *fp);
-	bool CreateSSCModule(FILE *fp, wxString &name);
-	bool RunSSCModule(FILE *fp, wxString &name);
-	bool FreeSSCModule(FILE *fp);
-	bool Input(FILE *fp, ssc_data_t p_data, const char *name, const wxString &folder, const int &array_matrix_threshold);
-	bool Output(FILE *fp, ssc_data_t p_data);
-	bool Footer(FILE *fp);
+	bool Header();
+	bool CreateSSCModule(wxString &name);
+	bool RunSSCModule(wxString &name);
+	bool FreeSSCModule();
+	bool Input(ssc_data_t p_data, const char *name, const wxString &folder, const int &array_matrix_threshold);
+	bool Output(ssc_data_t p_data);
+	bool Footer();
 };
 
 class CodeGen_matlab : virtual public CodeGen_Base
 {
 public:
 	CodeGen_matlab(Case *cc, const wxString &folder);
-	bool Header(FILE *fp);
-	bool CreateSSCModule(FILE *fp, wxString &name);
-	bool RunSSCModule(FILE *fp, wxString &name);
-	bool FreeSSCModule(FILE *fp);
-	bool Input(FILE *fp, ssc_data_t p_data, const char *name, const wxString &folder, const int &array_matrix_threshold);
-	bool Output(FILE *fp, ssc_data_t p_data);
-	bool Footer(FILE *fp);
+	bool Header();
+	bool CreateSSCModule(wxString &name);
+	bool RunSSCModule(wxString &name);
+	bool FreeSSCModule();
+	bool Input(ssc_data_t p_data, const char *name, const wxString &folder, const int &array_matrix_threshold);
+	bool Output(ssc_data_t p_data);
+	bool Footer();
 };
 
 #endif
