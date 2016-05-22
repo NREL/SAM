fc95dcbac10cb13e5bd3c9ec296cb78aaddb1040
Steve Janzou <steven.janzou@nrel.gov>
2016-05-22 12:31:54 +0000

    Update codegenerator to remove linux build warnings

--- src/case.h
+++ src/case.h
@@ -8,7 +8,7 @@
 #include "equations.h"
 #include "simulation.h"
 #include "excelexch.h"
-#include "codegen.h"
+#include "codegenerator.h"
 #include "parametric.h"
 #include "stochastic.h"
 #include "graph.h"
