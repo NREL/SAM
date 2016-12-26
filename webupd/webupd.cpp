59985849426a522c960d60c11d6f51f10eccd7ec
Steve Janzou <steven.janzou@nrel.gov>
2016-12-26 11:57:32 +0000

    Update version number for 2016.12.29 beta release

--- webupd/webupd.cpp
+++ webupd/webupd.cpp
@@ -31,7 +31,7 @@
 #include "pkgdlg.h"
 
 static wxString g_updateURL("https://sam.nrel.gov/sites/sam.nrel.gov/files/content/updates/");
-static wxString g_samVerIdStr("2016.10.25");
+static wxString g_samVerIdStr("2016.12.29");
 static wxString g_appPath;
 static wxString g_icmStr; // internet connection method string
 
