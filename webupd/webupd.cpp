b0fea0ea902556c77d120c558f27f5d117ce5505
Steve Janzou <steven.janzou@nrel.gov>
2017-02-15 02:16:12 +0000

    2017.2.14 beta release for Mark - no expiration...

--- webupd/webupd.cpp
+++ webupd/webupd.cpp
@@ -31,7 +31,7 @@
 #include "pkgdlg.h"
 
 static wxString g_updateURL("https://sam.nrel.gov/sites/default/files/content/updates/");
-static wxString g_samVerIdStr("2017.1.17");
+static wxString g_samVerIdStr("2017.2.14");
 static wxString g_appPath;
 static wxString g_icmStr; // internet connection method string
 
