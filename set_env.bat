3218dd6f822f5e7f35f26ce8631271a84b202dbf
Nick DiOrio <nadiorio@gmail.com>
2017-04-20 17:56:22 -0600

    Update environment script

--- set_env.bat
+++ set_env.bat
@@ -2,6 +2,6 @@ setx LKDIR C:\Projects\GitHub\SAM-private\lk\
 setx WEXDIR C:\Projects\GitHub\SAM-private\wex\
 setx SSCDIR C:\Projects\GitHub\SAM-private\ssc\
 setx SAMNTDIR C:\Projects\GitHub\SAM-private\SAMnt\
-setx WXMSW3 "c:\wxWidgets-3.1.0"
+setx WXMSW3 C:\wxWidgets-3.1.0
 
 	
