cb7f23dd479cf44fd348f92591206211f13abf98
Nick DiOrio <nadiorio@gmail.com>
2017-06-07 16:47:05 -0600

    Revert "Failed attempt at included Google Test framework"
    This reverts commit 74e2b25b908b32d1daa78c94985e4c952d9ab1dc.

--- set_env.bat
+++ set_env.bat
@@ -2,6 +2,6 @@ setx LKDIR C:\Users\Paul\Documents\GitHub\SAM-private\lk\
 setx WEXDIR C:\Users\Paul\Documents\GitHub\SAM-private\wex\
 setx SSCDIR C:\Users\Paul\Documents\GitHub\SAM-private\ssc\
 setx SAMNTDIR C:\Users\Paul\Documents\GitHub\SAM-private\SAMnt\
-setx WXMSW3 C:\wxWidgets-3.1.0\
-setx GTEST_ROOT C:\Projects\googletest\googletest\
+setx WXMSW3 C:\wxWidgets-3.1.0
+
 	
