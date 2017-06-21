59e023f86e33c0bcce5c661cad5b1f012a165065
Nick DiOrio <nicholas.diorio@nrel.gov>
2017-06-21 09:57:04 -0600

    Revert "Remove linux build files"
    This reverts commit 64fc14bd25daa6fdac0ed8fbdfafcb7ef6f126be.

---
+++ build_linux/sam.deploy/runtime/help/html/hmcontextids.js
@@ -0,0 +1,14 @@
+var hmContextIds = new Array();
+function hmGetContextId(query) {
+    var urlParams;
+    var match,
+        pl = /\+/g,
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+    params = {};
+    while (match = search.exec(query))
+       params[decode(match[1])] = decode(match[2]);
+    if (params["contextid"]) return decodeURIComponent(hmContextIds[params["contextid"]]);
+    else return "";
+}
+
