90b18744b3b61ea86e04a7aac86e0ef8f9c06d27
Nick DiOrio <nicholas.diorio@nrel.gov>
2017-06-21 09:46:11 -0600

    Delete all non-SAMnt files

--- ssc/tcs/fmin_callbacks.cpp
+++
@@ -1,9 +0,0 @@
-//#include "fmin_callbacks.h"
-//#include "sco2_power_cycle.h"
-//
-//double fmin_callback_opt_eta(double x, void *data)
-//{
-//	RecompCycle *frame = static_cast<RecompCycle*>(data);
-//
-//	return frame->opt_eta(x);
-//}
