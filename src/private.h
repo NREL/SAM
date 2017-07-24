4ac44a1c0d2b51ac9dc683904421768fa3fb55ff
sjanzou <sjanzou@gmail.com>
2017-07-23 21:44:42 -0600

    SAM opensource branch all files updated for public version

--- src/private.h
+++ src/private.h
@@ -58,30 +58,14 @@
 static const char *version_label = 0; //"iscc-ge"; 
 
 // API keys for SAM to use with developer.nrel.gov services.
-const char *sam_api_key =
-"rJzFOTOJhNHcLOnPmW2TNCLV8I4HHLgKddAycGpn"   // production (sam.support@nrel.gov)
-//"yXv3dcb6f5piO0abUMrrTuQvLDFgWvnBz52TJmDJ" // staging (aron.dobos@nrel.gov)
-;
-
+// request at https://developer.nrel.gov/signup/
+const char *sam_api_key = ""; // 40 character key
 
 // Google APIs:
+// geocoding at https://console.developers.google.com/apis/api/geocoding_backend/overview
 // login to developer api console at: https://code.google.com/apis/console
-// user name: aron.dobos@nrel.gov
-// passwd: 1H*****....******r
-static const char *GOOGLE_API_KEY = "AIzaSyCyH4nHkZ7FhBK5xYg4db3K7WN-vhpDxas";
+static const char *GOOGLE_API_KEY = "";
 
 // Bing Map APIs:
 // login to account center at: https://www.bingmapsportal.com/
-// user name: aron.dobos@nrel.gov
-// passwd: 1H*****....******r
-static const char *BING_API_KEY = "Av0Op8DvYGR2w07w_771JLum7-fdry0kBtu3ZA4uu_9jBJOUZgPY7mdbWhVjiORY";
-
-
-static const char *beta_disclaimer =
-"Notice: Beta versions of SAM are provided as-is and may change without notice."
-	"  Beta software may not work in the same way as a final version, and features and functionality may be changed, enhanced, or removed without notice."
-	"  The software is considered generally stable and useful but may produce incorrect results, crash, or behave otherwise unexpectedly."
-	"  There is no guarantee that files opened, saved, or created with this beta software will be usable with other versions of SAM."
-	"  This notice appears in addition to the full disclaimer of warranty, accessible in Help/About."
-	"\n\nThank you for trying SAM Beta.  We look forward to your feedback.";
-
+static const char *BING_API_KEY = "";
