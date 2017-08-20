8b53e6151df193b2dfe3171368ff5fe0145f73c7
sjanzou <steven.janzou@nrel.gov>
2017-08-20 03:12:29 -0600

    JSON inputs with metric outputs and compute modules

--- src/codegenerator.cpp
+++ src/codegenerator.cpp
@@ -7523,7 +7523,7 @@ bool CodeGen_android::Footer()
 CodeGen_json::CodeGen_json(Case *cc, const wxString &folder) : CodeGen_Base(cc, folder)
 {
 	m_num_cm = 0;
-	m_num_outputs = 0;
+	m_num_metrics = 0;
 }
 
 
@@ -7533,8 +7533,9 @@ bool CodeGen_json::Output(ssc_data_t )
 	for (size_t ii = 0; ii < m_data.size(); ii++)
 	{
 		const char *name = (const char*)m_data[ii].var.c_str();
-		fprintf(m_fp, "	\"output_%d\" : \"%s\",\n", m_num_outputs, name);
-		m_num_outputs++;
+		fprintf(m_fp, "	\"metric_%d\" : \"%s\",\n", m_num_metrics, name);
+		fprintf(m_fp, "	\"metric_%d_label\" : \"%s\",\n", m_num_metrics, (const char*)m_data[ii].label.c_str());
+		m_num_metrics++;
 	}
 	return true;
 }
@@ -7634,7 +7635,7 @@ bool CodeGen_json::SupportingFiles()
 bool CodeGen_json::Footer()
 {
 	fprintf(m_fp, "	\"number_compute_modules\" : %d,\n", m_num_cm);
-	fprintf(m_fp, "	\"number_outputs\" : %d\n", m_num_outputs);
+	fprintf(m_fp, "	\"number_metrics\" : %d\n", m_num_metrics);
 	fprintf(m_fp, "}\n");
 	return true;
 }
