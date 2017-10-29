740e3e6abff7660e671893cc30552e43425806d8
Ben Elliston <bje@air.net.au>
2017-10-29 17:09:38 +1100

    * src/invoke.cpp (fcall_dview_solar_data_file): Fix warning: unused   variable ‘err’.

--- src/invoke.cpp
+++ src/invoke.cpp
@@ -169,7 +169,7 @@ static void fcall_dview_solar_data_file( lk::invoke_t &cxt )
 	ssc_data_set_string(pdata, "file_name", (const char*)file.c_str());
 	ssc_data_set_number(pdata, "header_only", 0);
 
-	if ( const char *err = ssc_module_exec_simple_nothread( "wfreader", pdata ) )
+	if ( ssc_module_exec_simple_nothread( "wfreader", pdata ) )
 	{
 		wxLogStatus("error scanning '" + file + "'");
 		cxt.result().assign(0.0);
