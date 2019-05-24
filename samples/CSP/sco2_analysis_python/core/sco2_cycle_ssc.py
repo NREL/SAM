69e3cc7784d3efad393f93a0fb27991fe445b3a8
tyneises <ty.neises@nrel.gov>
2019-05-24 15:20:14 -0500

    add compressor nondimensional parameter sco2 cycle outputs to ssc post processing code

--- samples/CSP/sco2_analysis_python/core/sco2_cycle_ssc.py
+++ samples/CSP/sco2_analysis_python/core/sco2_cycle_ssc.py
@@ -136,6 +136,9 @@ def get_des_od_label_unit_info__calc_metrics():
     info["MC_phi"] = C_des_od_label_unit_info("mc_phi_des", "mc_phi_od", "Main Comp\nFlow Coef [-]", "Main Compressor Flow Coefficient [-]", "[-]")
     info["MC_phi"].limit_var = "mc_phi_surge"
     info["MC_phi"].limit_var_type = "min"
+    info["MC_psi"] = C_des_od_label_unit_info("mc_psi_des", "mc_psi_od", "Main Comp\nIdeal Head Coef [-]", "Main Compressor Ideal Head Coef[-]", "[-]")
+    info["MC_psi"].limit_var = "mc_psi_max_at_N_des"
+    info["MC_psi"].limit_var_type = "max"
     info["MC_tip_speed"] = C_des_od_label_unit_info("mc_tip_ratio_des", "mc_tip_ratio_od", "Main Comp\nTip Speed Ratio [-]", "Main Compressor Tip Speed Ratio [-]", "[-]")
     info["MC_tip_speed"].des_d_type = "list"
     info["MC_tip_speed"].od_d_type = "matrix"
@@ -158,6 +161,10 @@ def get_des_od_label_unit_info__calc_metrics():
     info["RC_phi"] = C_des_od_label_unit_info("rc_phi_des", "rc_phi_od", "Re-Comp\nFlow Coef [-]", "Re-Compressor Flow Coefficient [-]", "[-]")
     info["RC_phi"].limit_var = "rc_phi_surge"
     info["RC_phi"].limit_var_type = "min"
+    info["RC_psi"] = C_des_od_label_unit_info("rc_psi_des", "rc_psi_od", "Re-Comp\nIdeal Head Coef [-]",
+                                              "Re-Compressor Ideal Head Coefficient [-]", "[-]")
+    info["RC_psi"].limit_var = "rc_psi_max_at_N_des"
+    info["RC_psi"].limit_var_type = "max"
     info["RC_tip_speed"] = C_des_od_label_unit_info("rc_tip_ratio_des", "rc_tip_ratio_od", "Re-Comp\nTip Speed Ratio [-]", "Re-Compressor Tip Speed Ratio [-]", "[-]")
     info["RC_tip_speed"].des_d_type = "list"
     info["RC_tip_speed"].od_d_type = "matrix"
