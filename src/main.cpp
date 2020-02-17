dc0f73afe07c17263976b702a258cafe63748bff
Steven Janzou <sjanzou@gmail.com>
2020-02-17 04:25:29 -0700

    2020.2.17 ssc 235 VS2019 beta release

--- src/main.cpp
+++ src/main.cpp
@@ -116,6 +116,7 @@ static SamApp::ver releases[] = {
 //intermediate version numbers are required in this list in order for the version upgrade script (versions.lk) to work correctly
 //please clarify the reason for the new version in a comment. Examples: public release, variable changes, internal release, public beta release, etc.
 //the top version should always be the current working version
+		{ 2020, 02, 17 }, //VS2019 beta release
 		{ 2020, 02, 14 }, //CSP beta release
 	{ 2020, 02, 02 }, //projected public release
 		{ 2020, 1, 17 }, //Updated Beta for release testing - expires 1/17/2021 ssc version 232
