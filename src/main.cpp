f4f6fa79342ec152e74c8bcf54e9bb409d1a0c1b
Paul Gilman <cpaulgilman@gmail.com>
2020-02-27 08:01:00 -0800

    Move batteries up in config tree, fix order of lifetime page in startup

--- src/main.cpp
+++ src/main.cpp
@@ -2945,9 +2945,9 @@ void ConfigDialog::PopulateTech()
 	
 	// Manually add groups here - eventually move to startup.lk
 	wxDataViewItem cont_pv = m_pTech->AppendContainer(wxDataViewItem(0), "Photovoltaic");
+	wxDataViewItem cont_batt = m_pTech->AppendContainer(wxDataViewItem(0), "Battery Storage");
 	wxDataViewItem cont_csp = m_pTech->AppendContainer(wxDataViewItem(0), "Concentrating Solar Power");
 	wxDataViewItem cont_me = m_pTech->AppendContainer(wxDataViewItem(0), "Marine Energy");
-	wxDataViewItem cont_batt = m_pTech->AppendContainer(wxDataViewItem(0), "Battery Storage"); // for batt_test
 
 	for( size_t i=0;i<m_tnames.Count();i++)
 	{
@@ -2960,8 +2960,8 @@ void ConfigDialog::PopulateTech()
 			m_pTech->AppendItem(cont_csp, L);
 		else if (TP.Find("ME") != wxNOT_FOUND)
 			m_pTech->AppendItem(cont_me, L);
-		else if (TP.Find("BATT") != wxNOT_FOUND) // for batt_test
-			m_pTech->AppendItem(cont_batt, L); // for batt_test
+		else if (TP.Find("BATT") != wxNOT_FOUND) 
+			m_pTech->AppendItem(cont_batt, L); 
 		else
 			m_pTech->AppendItem(wxDataViewItem(0), L);
 	}
