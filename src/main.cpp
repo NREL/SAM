ff709ed6dea757eb3891155a47a5fe73aeb86375
dguittet <dguittet@nrel.gov>
2020-04-01 15:26:18 -0600

    add jsoncpp & python to README, fix encoding in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2421,7 +2421,7 @@ public:
 			"<font color=#a9a9a9 face=\"Segoe UI Light\" size=10>System Advisor Model (Open Source)</font><br><p>"
 				"Copyright 2017 Alliance for Sustainable Energy, LLC<br>"
 
-"NOTICE: This software was developed at least in part by Alliance for Sustainable Energy, LLC (�Alliance�) under Contract No. DE-AC36-08GO28308 with the U.S. Department of Energy and the U.S. The Government retains for itself and others acting on its behalf a nonexclusive, paid-up, irrevocable worldwide license in the software to reproduce, prepare derivative works, distribute copies to the public, perform publicly and display publicly, and to permit others to do so.<br><br>"
+    "NOTICE: This software was developed at least in part by Alliance for Sustainable Energy, LLC (\"Alliance\") under Contract No. DE-AC36-08GO28308 with the U.S. Department of Energy and the U.S. The Government retains for itself and others acting on its behalf a nonexclusive, paid-up, irrevocable worldwide license in the software to reproduce, prepare derivative works, distribute copies to the public, perform publicly and display publicly, and to permit others to do so.<br><br>"
 
 "Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:<br><br>"
 
@@ -2431,7 +2431,7 @@ public:
 
 "3. The entire corresponding source code of any redistribution, with or without modification, by a research entity, including but not limited to any contracting manager/operator of a United States National Laboratory, any institution of higher learning, and any non-profit organization, must be made publicly available under this license for as long as the redistribution is made available by the research entity.<br><br>"
 
-"4. Redistribution of this software, without modification, must refer to the software by the same designation. Redistribution of a modified version of this software (i) may not refer to the modified version by the same designation, or by any confusingly similar designation, and (ii) must refer to the underlying software originally provided by Alliance as �System Advisor Model� or �SAM�. Except to comply with the foregoing, the terms �System Advisor Model�, �SAM�, or any confusingly similar designation may not be used to refer to any modified version of this software or any modified version of the underlying software originally provided by Alliance without the prior written consent of Alliance.<br><br>"
+"4. Redistribution of this software, without modification, must refer to the software by the same designation. Redistribution of a modified version of this software (i) may not refer to the modified version by the same designation, or by any confusingly similar designation, and (ii) must refer to the underlying software originally provided by Alliance as \"System Advisor Model\" or \"SAM\". Except to comply with the foregoing, the terms \"System Advisor Model\", \"SAM\", or any confusingly similar designation may not be used to refer to any modified version of this software or any modified version of the underlying software originally provided by Alliance without the prior written consent of Alliance.<br><br>"
 
 "5. The name of the copyright holder, contributors, the United States Government, the United States Department of Energy, or any of their employees may not be used to endorse or promote products derived from this software without specific prior written permission.<br><br>"
 
