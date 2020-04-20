1292fe5ec0d9d5aec76b9359ded207433de3e4b9
Brian Mirletz <brian.mirletz@nrel.gov>
2020-04-20 14:08:21 -0600

    add python script for comparing command line lk output

---
+++ test/compare_lk_test_output.py
@@ -0,0 +1,59 @@
+import csv
+import sys
+
+annual_energy = "ANNUAL_OUTPUT"
+lcoe = "LCOE_NOM"
+lppa = "LPPA_NOM"
+
+def csv_to_dict(csvfile):
+    values = {}
+    for row in csvfile:
+        data = {}
+        key = str(row[0]) + " " + str(row[1])
+        if key != "TECHNOLOGY FINANCING": # Skip first row
+            data[annual_energy] = row[2]
+            data[lcoe] = row[3]
+            data[lppa] = row[4]
+            values[key] = data
+    return values
+
+if __name__ == "__main__":
+    expected_filename = sys.argv[1]
+    actual_filename = sys.argv[2]
+    
+    with open(expected_filename, "r", newline='') as datafile:
+        expected_csv = csv.reader(datafile, delimiter=',', quotechar='\"')
+        expected_values = csv_to_dict(expected_csv)
+        
+    with open(actual_filename, "r", newline='') as datafile:
+        actual_csv = csv.reader(datafile, delimiter=',', quotechar='\"')
+        actual_values = csv_to_dict(actual_csv)
+        
+    success = True
+    error = 0.001
+    for key in expected_values:
+        ex_data = expected_values[key]
+        act_data = actual_values[key]
+        if act_data:
+            for k in ex_data:
+                if act_data[k] == "fail":
+                    success = False
+                    print("Key " + key + " failed")
+                elif act_data[k] == ex_data[k]:
+                    continue
+                else:
+                    act = float(act_data[k])
+                    ex = float(ex_data[k])
+                    if abs((ex - act) / ex) > error:
+                        success = False
+                        print("Large change in " + k + " for " + key + ". Expected " + ex_data[k] + " / Actual: " + act_data[k])
+                           
+        else:
+            success = False
+            print("Key " + key + " missing in test results " + actual_filename)
+    
+    if success:
+        print("Success!")
+    else:
+        sys.exit(1)   
+        
