bc50be3efdccdaea27f498bb7518ee4b67956dcc
Steven Janzou <sjanzou@gmail.com>
2021-03-25 05:38:46 -0600

    EPRI 2021.3.25 ssc 254 beta release

--- src/main.cpp
+++ src/main.cpp
@@ -117,6 +117,7 @@ static SamApp::ver releases[] = {
 //intermediate version numbers are required in this list in order for the version upgrade script (versions.lk) to work correctly
 //please clarify the reason for the new version in a comment. Examples: public release, variable changes, internal release, public beta release, etc.
 //the top version should always be the current working version
+		{ 2021, 3, 25 }, // 2021.3.25 ssc 254 EPRI Beta expires 3/25/2022
 		{ 2021, 3, 8 }, // 2021.3.8 ssc 253 EPRI Beta expires 3/8/2022
 		{ 2021, 2, 24 }, // 2021.2.24 ssc 251 release
 		{ 2020, 11, 29 }, // 2020.11.29 ssc 250 release
