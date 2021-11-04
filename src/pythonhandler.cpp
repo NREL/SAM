3671259c59817affd051975d8b494b7c745c17ec
Brian Mirletz <brian.mirletz@nrel.gov>
2021-11-04 12:13:51 -0600

    Revert "Merge pull request #726 from NREL/save_as_JSON"
    This reverts commit 7d2b81b0862b3427acecaea8cf0eeed72a6df5e4, reversing
    changes made to 417fe219174a94e99a7ba17cd5385918db63c28d.

--- src/pythonhandler.cpp
+++ src/pythonhandler.cpp
@@ -23,8 +23,6 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <unordered_map>
 #include <future>
 #include <algorithm>
-#include <sstream>
-
 
 #ifdef __WXMSW__
 #pragma warning(disable: 4191)
@@ -33,20 +31,13 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endif
 
 #include "pythonhandler.h"
-#include <ssc/../rapidjson/document.h>
-#include <ssc/../rapidjson/istreamwrapper.h>
-#include <ssc/../rapidjson/stringbuffer.h>
-#include <ssc/../rapidjson/prettywriter.h>
-
-
 
 PythonConfig ReadPythonConfig(const std::string& configPath) {
     // load python configuration
-    rapidjson::Document python_config_root;
+    Json::Value python_config_root;
     std::ifstream python_config_doc(configPath);
     if (python_config_doc.fail())
         throw std::runtime_error("Could not open " + configPath );
-
 
 #ifdef __WXMSW__
 	// check for byte-order mark indicating UTF-8 and skip if it exists since it's not JSON-compatible
@@ -59,38 +50,36 @@ PythonConfig ReadPythonConfig(const std::string& configPath) {
 	}
 #endif
 
-    std::ostringstream tmp;
-    tmp << python_config_doc.rdbuf();
-    python_config_root.Parse(tmp.str().c_str());
+    python_config_doc >> python_config_root;
 
-    if (!python_config_root.HasMember("miniconda_version"))
+    if (!python_config_root.isMember("miniconda_version"))
         throw std::runtime_error("Missing key 'miniconda_version' in " + configPath);
-    if (!python_config_root.HasMember("python_version"))
+    if (!python_config_root.isMember("python_version"))
         throw std::runtime_error("Missing key 'python_version' in " + configPath);
-    if (!python_config_root.HasMember("exec_path"))
+    if (!python_config_root.isMember("exec_path"))
         throw std::runtime_error("Missing key 'exec_path' in " + configPath);
-    if (!python_config_root.HasMember("pip_path"))
+    if (!python_config_root.isMember("pip_path"))
         throw std::runtime_error("Missing key 'pip_path' in " + configPath);
-    if (!python_config_root.HasMember("packages"))
+    if (!python_config_root.isMember("packages"))
         throw std::runtime_error("Missing key 'packages' in " + configPath);
 
     std::vector<std::string> packages;
-    for (auto &i : python_config_root["packages"].GetArray())
-        packages.push_back(i.GetString());
+    for (auto &i : python_config_root["packages"])
+        packages.push_back(i.asString());
 
     std::unordered_map<std::string, std::string> options;
- 
-    if (python_config_root.HasMember("options")){
-        rapidjson::Value json_val = python_config_root["options"].GetObject();
-        for (rapidjson::Value::ConstMemberIterator itr = json_val.MemberBegin(); itr != json_val.MemberEnd(); ++itr) {
-            options.insert({ itr->name.GetString(),itr->value.GetString() });
+    if (python_config_root.isMember("options")){
+        Json::Value json_val = python_config_root["options"];
+        Json::Value::Members members = json_val.getMemberNames();
+        for (auto const &name : members) {
+            options.insert({name, json_val[name].asString()});
         }
     }
 
-    PythonConfig config = {python_config_root["python_version"].GetString(),
-                           python_config_root["miniconda_version"].GetString(),
-                           python_config_root["exec_path"].GetString(),
-                           python_config_root["pip_path"].GetString(),
+    PythonConfig config = {python_config_root["python_version"].asString(),
+                           python_config_root["miniconda_version"].asString(),
+                           python_config_root["exec_path"].asString(),
+                           python_config_root["pip_path"].asString(),
                            packages,
                            options};
 
@@ -101,32 +90,20 @@ void WritePythonConfig(const std::string& configPath, const PythonConfig& config
     std::ofstream configFile;
     configFile.open(configPath);
 
-    rapidjson::Document configObj;
-    configObj.SetObject();
-    configObj.AddMember(rapidjson::Value("python_version", configObj.GetAllocator()).Move(), rapidjson::Value(config.pythonVersion.c_str(), configObj.GetAllocator()).Move(), configObj.GetAllocator());
-    configObj.AddMember(rapidjson::Value("miniconda_version", configObj.GetAllocator()).Move(), rapidjson::Value(config.minicondaVersion.c_str(), configObj.GetAllocator()).Move(), configObj.GetAllocator());
-    configObj.AddMember(rapidjson::Value("exec_path", configObj.GetAllocator()).Move(), rapidjson::Value(config.execPath.c_str(), configObj.GetAllocator()).Move(), configObj.GetAllocator());
-    configObj.AddMember(rapidjson::Value("pip_path", configObj.GetAllocator()).Move(), rapidjson::Value(config.pipPath.c_str(), configObj.GetAllocator()).Move(), configObj.GetAllocator());
-    // array
-    rapidjson::Value json_val;
-    json_val.SetArray();
-    for (auto& i : config.packages) {
-        json_val.PushBack(rapidjson::Value(i.c_str(), configObj.GetAllocator()), configObj.GetAllocator());
-    }
-    configObj.AddMember(rapidjson::Value("packages", configObj.GetAllocator()).Move(), json_val.Move(), configObj.GetAllocator());
+    Json::Value configObj;
+    configObj["python_version"] = config.pythonVersion;
+    configObj["miniconda_version"] = config.minicondaVersion;
+    configObj["exec_path"] = config.execPath;
+    configObj["pip_path"] = config.pipPath;
+    configObj["packages"] = Json::arrayValue;
+    for (auto &i : config.packages)
+        configObj["packages"].append(i);
+    for (auto &i : config.options)
+        configObj["options"][i.first] = i.second;
 
-    json_val.Clear();
-    json_val.SetObject();
-    for (auto& i : config.options) {
-        json_val.AddMember(rapidjson::Value(i.first.c_str(), configObj.GetAllocator()).Move(), rapidjson::Value(i.second.c_str(), configObj.GetAllocator()).Move(), configObj.GetAllocator());
-    }
-    configObj.AddMember(rapidjson::Value("options", configObj.GetAllocator()).Move(), json_val.Move(), configObj.GetAllocator());
-
-     rapidjson::StringBuffer buffer;
-    buffer.Clear();
-    rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(buffer);
-    configObj.Accept(writer);
-    configFile << buffer.GetString();
+    Json::StreamWriterBuilder builder;
+    std::unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
+    writer -> write(configObj, &configFile);
     configFile.close();
 }
 
@@ -148,27 +125,24 @@ bool InstallPythonUnix(const std::string& path, const PythonConfig& config){
 
 PythonPackageConfig ReadPythonPackageConfig(const std::string& name, const std::string& configFile){
     // load python configuration
-    rapidjson::Document python_config_root;
+    Json::Value python_config_root;
     std::ifstream python_config_doc(configFile);
     if (python_config_doc.fail())
         throw std::runtime_error("Could not open " + configFile );
 
-    std::ostringstream tmp;
-    tmp << python_config_doc.rdbuf();
-    python_config_root.Parse(tmp.str().c_str());
+    python_config_doc >> python_config_root;
 
-
-    if (!python_config_root.HasMember("min_python_version"))
+    if (!python_config_root.isMember("min_python_version"))
         throw std::runtime_error("Missing key 'min_python_version' in " + configFile);
-    if (!python_config_root.HasMember("run_cmd"))
+    if (!python_config_root.isMember("run_cmd"))
         throw std::runtime_error("Missing key 'run_cmd' in " + configFile);
-    if (!python_config_root.HasMember("version"))
+    if (!python_config_root.isMember("version"))
         throw std::runtime_error("Missing key 'version' in " + configFile);
 
     PythonPackageConfig config = {name,
-                                  python_config_root["min_python_version"].GetString(),
-                                  python_config_root["run_cmd"].GetString(),
-                                  python_config_root["version"].GetString()};
+                                  python_config_root["min_python_version"].asString(),
+                                  python_config_root["run_cmd"].asString(),
+                                  python_config_root["version"].asString()};
 
     return config;
 }
