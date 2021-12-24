95ccd3e137662be9f7f5f0acc156eb13c6ef6a3e
dguittet <dguittet@nrel.gov>
2021-12-24 10:07:34 -0700

    add PySSC differ to run_defaults_diff.py

--- test/run_defaults_diff.py
+++ test/run_defaults_diff.py
@@ -1,51 +1,73 @@
-import json
-import os
-import tempfile
+from PySAM.PySSC import PySSC
 import requests
-import tarfile
-import shutil
-from collections.abc import Iterable
 from collections import OrderedDict
+from collections.abc import Iterable
+import tempfile
+import os
+import sys
+import json
+import tarfile
+from ctypes import *
 
-#################################################################################################################
-#
-# This file compares the SSC Compute Modules input variables and the SAM defaults for these input variables
-# between the current branch and the previous SAM Release. This helps track which variables and defaults have
-# changed between SAM, SDK and PySAM releases to make it easier to notify users during version upgrade.
-#
-# The output JSON contains the following nested dictionaries: 
-#    1. 'Cmods with new defaults files'
-#           Cmods listed here have new default configuration files in the new Release. This may occur with a new
-#           SAM configuration, or if an existing SAM configuration added or switched a cmod, e.g. PVWatts using 
-#           v8 instead of v7
-#    2. 'Cmods with removed defaults files'
-#           Cmods listed here have deleted default configuration files in the new Release. This may occur with a
-#           deleted SAM configuration.
-#    3. 'Cmods with modified variables'
-#           There are three categories here: 'Added variables', 'Removed variables' and 'Type changed'.
-#    4. 'Configs with modified defaults'
-#           This is records the default values that have changed. For variables whose lengths are greater than or
-#           equal to 8760, the value changes are only reported for the first index to save space.
-#
-# Note: Since this file uses the Default files to search for variable changes, and Default files do not exist
-#    for cmods not in a SAM configuration (e.g. modules providing helper functions or older versions, listed 
-#    here: https://nrel-pysam.readthedocs.io/en/master/Models.html#other-modules-names-and-descriptions), for 
-#    these cmods, variable changes will not be tracked by this current method. 
-#
-#################################################################################################################
 
+"""
+      This Script compares the SSC input variables and the defaults for these input variables
+      between the current branch and the last release. This helps track which variables
+      and defaults have changed between releases to make it easier to notify users during version upgrade.
+      Below are lists for each Compute Module:
+
+          1. New Default configurations
+
+             Cmods have new default configuration files in the new Release. This may occur with a new
+             SAM configuration, or if an existing SAM configuration added or switched a cmod, e.g. PVWatts using 
+             v8 instead of v7
+
+          2. Removed Default configurations
+
+             Cmods have deleted default configuration files in the new Release. This may occur with a deleted SAM
+             configuration.
+
+          3. Modified Variables: New variables, Removed variables and Type-changed variables
+
+          4. Modified Default Values
+
+             This is records the default values that have changed. For variables whose lengths are greater than or
+             equal to 8760, the value changes are only reported for the first index to save space.
+          
+      To Use: Provide as arguments to the script
+          1. Path to the old ssc library
+          2. Path to the new ssc library
+"""
+
+
+if len(sys.argv) < 3:
+    raise RuntimeError("Please provide the path to the old Release's ssc library and the path to the new library")
+old_ssc = sys.argv[1]
+if not os.path.exists(old_ssc):
+    raise RuntimeError("Path to old Release's ssc library was invalid")
+new_ssc = sys.argv[2]
+if not os.path.exists(new_ssc):
+    raise RuntimeError("Path to old Release's ssc library was invalid")
+print(f"Comparing SSC libraries from old at {old_ssc} to new at {new_ssc}")
+
+
+########################################################################################
 #
-# Download the previous SAM Release from Github's Release API
-# Set to 0 to compare with the latest release; gets the previous release if 1
+# Download the SAM repo tagged for the previous Release to compare defaults
 #
-previous_release = 1
+########################################################################################
+
+previous_release = 0    # set to 1 to compare against previous before last
 resp = requests.get("https://api.github.com/repos/NREL/sam/releases").json()
 old_release = resp[previous_release]['tarball_url']
-print(f"Comparing Current branch's SSC Variables with Release `{resp[previous_release]['name']}` published at {resp[previous_release]['created_at']}")
+
+print(
+    f"Comparing Current branch's SSC Defaults with Release `{resp[previous_release]['name']}` published at "
+    f"{resp[previous_release]['created_at']}")
 
 tmpdir = tempfile.TemporaryDirectory()
 print(f'Using temporary dir {tmpdir.name}')
-with requests.get(old_release, stream = True) as File:
+with requests.get(old_release, stream=True) as File:
     # stream = true is required by the iter_content below
     sam_old_file = os.path.join(tmpdir.name, "sam_old")
     with open(sam_old_file, 'wb') as fd:
@@ -70,9 +92,11 @@ for root, dirs, files in os.walk(api_path):
 
 outfile_dict = {}
 
+########################################################################################
 #
 # Get changed default files
 #
+########################################################################################
 
 defaults_new = set([os.path.splitext(os.path.basename(i))[0] for i in file_list_new])
 defaults_old = set([os.path.splitext(os.path.basename(i))[0] for i in file_list_old])
@@ -99,9 +123,6 @@ newly_removed_defaults_dict.keys()
 
 outfile_dict["Cmods with removed defaults files"] = newly_removed_defaults_dict
 
-#
-# Get variable changes
-#
 
 def get_flat_dict(defaults_json):
     output = {}
@@ -115,10 +136,12 @@ def get_flat_dict(defaults_json):
             output[k] = v
     return output
 
+
 def file_to_flat_dict(filename):
     with open(filename, 'r') as f:
         defaults_json = json.load(f)
         return get_flat_dict(defaults_json)
+
 
 new_defaults_data = {}
 for f in file_list_new:
@@ -131,7 +154,6 @@ for f in file_list_old:
     file_name = os.path.split(f)[1]
     old_defaults_data[file_name] = file_to_flat_dict(f)
 
-outfile_dict["Cmods with modified variables"] = {}
 outfile_dict["Configs with modified defaults"] = {}
 
 for k, new_data in new_defaults_data.items():
@@ -139,43 +161,17 @@ for k, new_data in new_defaults_data.items():
         name = k.split('_')[0]
         config_name = k.split('.')[0]
         old_data = old_defaults_data[k]
-        
+
         # First, compare sets
         new_key_set = set(new_data.keys())
         old_key_set = set(old_data.keys())
-        
-        addtl_keys = new_key_set.difference(old_key_set)
-        
-        if (len(addtl_keys) > 0):
-            if name not in outfile_dict["Cmods with modified variables"].keys():
-                outfile_dict["Cmods with modified variables"][name] = {'Added variables': []}
-            for ak in addtl_keys:
-                if ak not in outfile_dict["Cmods with modified variables"][name]['Added variables']:
-                    outfile_dict["Cmods with modified variables"][name]['Added variables'].append(ak)
-            
-        removed_keys = old_key_set.difference(new_key_set)
-        
-        if (len(removed_keys) > 0):
-            if name not in outfile_dict["Cmods with modified variables"].keys():
-                outfile_dict["Cmods with modified variables"][name] = {'Removed variables': []}
-            if 'Removed variables' not in outfile_dict["Cmods with modified variables"][name].keys():
-                outfile_dict["Cmods with modified variables"][name]['Removed variables'] = []
-            for rk in removed_keys:
-                if rk not in outfile_dict["Cmods with modified variables"][name]['Removed variables']:
-                    outfile_dict["Cmods with modified variables"][name]['Removed variables'].append(rk)
-                
+
         all_keys = new_key_set.intersection(old_key_set)
         changed_keys = {}
         for key in all_keys:
             v_new = new_data[key]
             v_old = old_data[key]
-            
-            if type(v_new) != type(v_old):
-                if not isinstance(v_new, (int, float)) or not isinstance(v_old, (int, float)):
-                    if name not in outfile_dict["Cmods with modified defaults files"].keys():
-                        outfile_dict["Cmods with modified variables"][name] = {'Type changed': []}
-                    if key not in [r[0] for r in outfile_dict["Cmods with modified variables"][name]['Type changed']]:
-                        outfile_dict["Cmods with modified variables"][name]['Type changed'] = [key, str(type(v_old)), str(type(v_new))]
+
             if v_new != v_old:
                 if isinstance(v_new, Iterable) and isinstance(v_old, Iterable):
                     if len(v_new) + len(v_old) >= 8760 * 2:
@@ -188,27 +184,107 @@ for k, new_data in new_defaults_data.items():
             if name not in outfile_dict["Configs with modified defaults"].keys():
                 outfile_dict["Configs with modified defaults"][config_name] = {}
             outfile_dict["Configs with modified defaults"][config_name]['Changed variables'] = changed_keys
-        # sort and order
-        if name not in outfile_dict["Cmods with modified variables"]:
-            continue
-        if 'Removed variables' in outfile_dict["Cmods with modified variables"][name].keys():
-            outfile_dict["Cmods with modified variables"][name]['Removed variables'].sort()
-        if 'Added variables' in outfile_dict["Cmods with modified variables"][name].keys():
-            outfile_dict["Cmods with modified variables"][name]['Added variables'].sort()
-        if 'Type changed' in outfile_dict["Cmods with modified variables"][name].keys():
-            outfile_dict["Cmods with modified variables"][name]['Type changed'].sort()
-            
-outfile_dict["Cmods with modified variables"] = OrderedDict(sorted(outfile_dict["Cmods with modified variables"].items()))
-outfile_dict["Configs with modified defaults"] = OrderedDict(sorted(outfile_dict["Configs with modified defaults"].items()))
 
+# Organize
+
+doc_dict = OrderedDict()
+cmods = set(outfile_dict['Cmods with new defaults files'].keys())
+cmods = cmods.union(set(outfile_dict['Cmods with removed defaults files'].keys()))
+cmods = list(cmods)
+cmods.sort()
+
+for c in cmods:
+    doc_dict[c] = {}
+    if c in outfile_dict['Cmods with new defaults files'].keys():
+        doc_dict[c]['new_defaults'] = outfile_dict['Cmods with new defaults files'][c]
+    if c in outfile_dict['Cmods with removed defaults files'].keys():
+        doc_dict[c]['del_defaults'] = outfile_dict['Cmods with removed defaults files'][c]
+    def_dict = {}
+    for k, v in outfile_dict['Configs with modified defaults'].items():
+        if c in k:
+            def_dict[k] = v['Changed variables']
+    if len(def_dict) > 0:
+        doc_dict[c]['mod_defaults'] = def_dict
+    if len(doc_dict[c]) == 0:
+        doc_dict.pop(c)
+
+########################################################################################
 #
-# Export, print and clean up
+# Get variable changes using PySSC to query Compute Modules' interfaces
 #
+########################################################################################
 
-outfile_name = "version_diffs.json"
 
-with open(outfile_name, "w") as out_file:
-    json.dump(outfile_dict, out_file, indent=4)
-print(json.dumps(outfile_dict, indent=4))
+def get_var_dict():
+    cmod_variables = {}
+    i = 0
+    while ssc.module_entry(i):
+        name = ssc.entry_name(ssc.module_entry(i))
+        mod = ssc.module_create(name)
+        j = 0
+        cmod_variables[name.decode("utf-8")] = {}
+        while ssc.module_var_info(mod, j):
+            var = ssc.module_var_info(mod, j)
+            vname = ssc.info_name(var).decode("utf-8")
+            vtype = ssc.info_var_type(var)
+            if vtype == 1 or vtype == 3:
+                cmod_variables[name.decode("utf-8")][vname] = ssc.info_data_type(var)
+            j += 1
+        i += 1
+    return cmod_variables
 
-shutil.rmtree(tmpdir.name)
+
+ssc = PySSC()
+ssc.pdll = CDLL(new_ssc)
+new_cmod_variables = get_var_dict()
+
+
+ssc = PySSC(old_ssc)
+ssc.pdll = CDLL(old_ssc)
+old_cmod_variables = get_var_dict()
+
+cmod_int = set(new_cmod_variables.keys()).intersection(set(old_cmod_variables.keys()))
+
+for name in cmod_int:
+    pysam_name = "".join([s.capitalize() for s in name.split('_')])
+    mod_variables = dict()
+
+    new_key_set = set(new_cmod_variables[name].keys())
+    old_key_set = set(old_cmod_variables[name].keys())
+
+    addtl_keys = new_key_set.difference(old_key_set)
+
+    if len(addtl_keys) > 0:
+        addtl_keys = list(addtl_keys)
+        addtl_keys.sort()
+        mod_variables['Added variables'] = addtl_keys
+
+    removed_keys = old_key_set.difference(new_key_set)
+
+    if len(removed_keys) > 0:
+        removed_keys = list(removed_keys)
+        removed_keys.sort()
+        mod_variables['Removed variables'] = removed_keys
+
+    all_keys = new_key_set.intersection(old_key_set)
+    changed_keys = {}
+    for key in all_keys:
+        v_new = new_cmod_variables[name][key]
+        v_old = old_cmod_variables[name][key]
+
+        if v_new != v_old:
+            if 'Type changed' not in mod_variables.keys():
+                mod_variables['Type changed'] = []
+            mod_variables['Type changed'].append(key)
+    if 'Type changed' in mod_variables.keys():
+        mod_variables['Type changed'].sort()
+    if len(mod_variables) > 0:
+        if pysam_name not in doc_dict.keys():
+            doc_dict[pysam_name] = {'mod_variables': mod_variables}
+        else:
+            doc_dict[pysam_name].update({'mod_variables': mod_variables})
+
+doc_dict = OrderedDict(sorted(doc_dict.items()))
+
+with open('version_diff.json', 'w') as f:
+    json.dump(doc_dict, f, indent=4)
