70fd386cbb40fbf43e0aa9c8ad256a8831172407
tyneises <ty.neises@nrel.gov>
2022-04-26 16:52:21 -0500

    add ptes to pysam
    change etes entry to reflect cmod name

--- api/api_autogen/data_structures.h
+++ api/api_autogen/data_structures.h
@@ -170,6 +170,7 @@ static std::unordered_map<std::string, std::string> config_to_cmod_name = {
         {"DSPT", "TcsdirectSteam"},
         {"EmpiricalTrough", "TcstroughEmpirical"},
         {"ETES", "EtesElectricResistance"},
+        {"PTES", "EtesPtes"},
         {"FlatPlatePV", "Pvsamv1"},
         {"FuelCell", "Fuelcell"},
         {"GenericCSPSystem", "TcsgenericSolar"},
