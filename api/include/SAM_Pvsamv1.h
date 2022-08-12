f4ed4af2ccc9937c2eaac602bb819a10baf39f2f
Paul Gilman <cpaulgilman@gmail.com>
2022-08-12 16:10:57 -0700

    PV land area UI cleanup (#1127)
    * Land area estimate fixes
    
    * Fix mistake in module area in hectares calculated value
    * Reorganize land area inputs
    
    * Add new defaults for UI land area variables
    
    These are calculated values in user interface, so set to zero by default. Correct value should be calculated when UI forms load.
    
    * Generate API files for new PV land area UI inputs and defaults

--- api/include/SAM_Pvsamv1.h
+++ api/include/SAM_Pvsamv1.h
@@ -4523,6 +4523,22 @@ extern "C"
 	SAM_EXPORT void SAM_Pvsamv1_SystemCosts_om_batt_replacement_cost_aset(SAM_table ptr, double* arr, int length, SAM_error *err);
 
 	/**
+	 * Set om_batt_variable_cost: Battery production-based System Costs amount [$/MWh]
+	 * options: None
+	 * constraints: None
+	 * required if: ?=0.0
+	 */
+	SAM_EXPORT void SAM_Pvsamv1_SystemCosts_om_batt_variable_cost_aset(SAM_table ptr, double* arr, int length, SAM_error *err);
+
+	/**
+	 * Set om_production_escal: Production-based O&M escalation [%/year]
+	 * options: None
+	 * constraints: None
+	 * required if: ?=0.0
+	 */
+	SAM_EXPORT void SAM_Pvsamv1_SystemCosts_om_production_escal_nset(SAM_table ptr, double number, SAM_error *err);
+
+	/**
 	 * Set om_replacement_cost_escal: Replacement cost escalation [%/year]
 	 * options: None
 	 * constraints: None
@@ -6240,6 +6256,10 @@ extern "C"
 	 */
 
 	SAM_EXPORT double* SAM_Pvsamv1_SystemCosts_om_batt_replacement_cost_aget(SAM_table ptr, int* length, SAM_error *err);
+
+	SAM_EXPORT double* SAM_Pvsamv1_SystemCosts_om_batt_variable_cost_aget(SAM_table ptr, int* length, SAM_error *err);
+
+	SAM_EXPORT double SAM_Pvsamv1_SystemCosts_om_production_escal_nget(SAM_table ptr, SAM_error *err);
 
 	SAM_EXPORT double SAM_Pvsamv1_SystemCosts_om_replacement_cost_escal_nget(SAM_table ptr, SAM_error *err);
 
