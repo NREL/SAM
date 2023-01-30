c84a86ad12ba719cbd9b41f437b1776b14d4d1ea
dguittet <dguittet@nrel.gov>
2023-01-30 11:23:20 -0700

    add line for INOUT

--- api/api_autogen/builder_PySAM.cpp
+++ api/api_autogen/builder_PySAM.cpp
@@ -427,6 +427,11 @@ void builder_PySAM::create_PySAM_files(const std::string &cmod, const std::strin
                         doc += "**Info:**\\n" + vd.meta;
                 }
 
+                if (vd.var_type == 3) {
+                    doc += "\\n\\n";
+                    doc += "**INOUT:** Also an Output variable.";
+                }
+
                 if (vd.constraints.length() > 0) {
                     doc += "\\n\\n";
                     doc += "**Constraints:**\\n";
