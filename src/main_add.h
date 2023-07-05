cf3e04743f931102c1c1eeb52adab504470de73e
Taylor Brown <taylor.brown75@outlook.com>
2023-07-05 12:27:52 -0600

    Add version upgrade script for new molten salt linear fresnel. Fix dispatch control UI file name.

--- src/main_add.h
+++ src/main_add.h
@@ -74,6 +74,7 @@ static SamApp::ver releases[] = {
 	//intermediate version numbers are required in this list in order for the version upgrade script (versions.lk) to work correctly
 	//please clarify the reason for the new version in a comment. Examples: public release, variable changes, internal release, public beta release, etc.
 	//the top version should always be the current working version
+			{ 2023, 6, 29 }, // 2023.6.29 DEBUG version to test the new linear fresnel model
 			{ 2022, 11, 21 }, // 2022.11.21 ssc 278 public release and ssc 279 revision 1
 			{ 2021, 12, 02 }, // 2021.12.02 ssc 267 public release
 			{ 2021, 11, 30 }, // 2021.11.30 ssc 265 release candidate beta expires 11/30/2022
