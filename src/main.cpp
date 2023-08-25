0f8abb301d084c7a64d8754931fa8ad0c2d9002a
Steven Janzou <steven@janzouconsulting.com>
2023-08-25 04:58:14 -0600

    Fix broken PVWatts-Wind-FuelCell-Battery configurations

--- src/main.cpp
+++ src/main.cpp
@@ -255,7 +255,7 @@ MainWindow::MainWindow()
 	entries.push_back( wxAcceleratorEntry( wxACCEL_SHIFT, WXK_F7,  ID_INTERNAL_IDE ) ) ;
 	entries.push_back( wxAcceleratorEntry( wxACCEL_SHIFT, WXK_F8,  ID_INTERNAL_RESTART ) );
 	entries.push_back( wxAcceleratorEntry( wxACCEL_SHIFT, WXK_F9, ID_INTERNAL_DATAFOLDER));
-	entries.push_back( wxAcceleratorEntry( wxACCEL_SHIFT, WXK_F1, ID_SAVE_CASE_DEFAULTS));
+	entries.push_back( wxAcceleratorEntry( wxACCEL_SHIFT, WXK_F10, ID_SAVE_CASE_DEFAULTS));
 	entries.push_back( wxAcceleratorEntry( wxACCEL_SHIFT, WXK_F11, ID_RUN_ALL_CASES));
 	entries.push_back( wxAcceleratorEntry( wxACCEL_SHIFT, WXK_F12, ID_INTERNAL_CASE_VALUES));
 	entries.push_back( wxAcceleratorEntry( wxACCEL_CTRL, WXK_F1, ID_INTERNAL_SEGFAULT));
