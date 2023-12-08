65a24cf2b6ec4d4e7d76bf24d54cee6f85c91a97
Paul Gilman <cpaulgilman@gmail.com>
2023-12-08 15:24:28 -0800

    Final Help revisions for SAM 2023.12.17 release
    Complete list of revisions since SAM 2022.11.21 r3:
    
    Change Model does not work for hybrid configurations.
    
    PV subhourly clipping correction
    
    Weather Data Elements required for PV module heat transfer cell model.
    
    Operating Cost descriptions for fuel cell and battery techs.
    
    Explain where to find incentives in cash flow.
    
    Revise REopt instructions for clarity and for new grid outage feature.
    
    CBI for hybrid systems based on total generation capacity.
    
    Add description of BTM battery dispatch self-consumption option.
    
    Rename BTM battery dispatch option "Price Signal Forecast" to "Retail Rate Dispatch".
    
    Clarify replacement cost as operating cost vs replacement reserve.
    
    PVWatts cell temperature model: https://github.com/NREL/SAM/issues/1521
    
    PVWatts remove mention of PVWatts Calculator using PVWatts V7.
    
    PVWatts note about premium, standard, thin film module type.
    
    Fix PVWatts link to physical IAM model description on PVPMC: https://github.com/NREL/SAM/issues/1508
    
    Incentves, remove 75% cliff for PV-battery storage from ITC description.
    
    Move PV system sizing and microinverter topics to System Design and Inverter topics, respectively.
    
    Hybrid System and Hybrid System Costs pages.
    
    CSP MSLF new System Design (copy of Physical trough, note no max net power to the grid (incl. avaialbility)
    
    Separate Detailed PV and PVWatts Location and Resource topics.
    
    Separate AC degradation and DC degradation topics.
    
    New Grid top-level topic for Grid Limits and Grid Outage.
    
    PVWatts add rated inverter size description
    
    New design for Help with logo
    
    Revise battery storage description
    
    Move all performance model chapters to top level:
    
    * Move "Custom HTF" to Solar Field topics for each relevant model
    
    * Move "Solar Multiple" topic to System Design page for each relevant model.
    
    * Move "Microinverters" to Inverter topic
    
    * Move PV Sizing topic to PV System Design
    
    * Move "Location and Resource" topic copy to each solar technology.
    
    Add topics for IPH DSLF and IPH MSPT
    
    Revise MSPT System Design move solar multiple to heliostat field section
    
    Revise keyboard shortcuts from main.cpp.
    
    Fix wave energy frequency of occurrence file format description.
    
    Fix bifacial PV ground clearance description. https://sam.nrel.gov/forum/forum-general/4330
    
    Fix solar water heating nameplate capacity description. https://sam.nrel.gov/forum/forum-general/4299-solar-water-heating-total-area-equation-units.html
    
    Fix link in Electricity Rates to billing definitions.
    
    Update ETES dispatch to explain how dispatch works without optimization.

--- deploy/runtime/help/html/js/hmXtopic.js
+++ deploy/runtime/help/html/js/hmXtopic.js
@@ -6,7 +6,7 @@ hmProjectInfo = {
 	copyright: "© 2023 National Renewable Energy Laboratory",
 	summary: "&nbsp;",
 	version: "0.0.1",
-	date: "Thursday, November 16, 2023",
+	date: "Friday, December 8, 2023",
 	language: "en-us",
 	mainfile: "index.html"
 	}
