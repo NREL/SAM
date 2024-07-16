9b654fdebac64ee8154034e9b8dcf951c80ecfd2
Paul Gilman <cpaulgilman@gmail.com>
2024-07-16 16:24:24 -0700

    Help revisions for depreciation basis update

--- deploy/runtime/help/html/js/hmXtopic.js
+++ deploy/runtime/help/html/js/hmXtopic.js
@@ -6,7 +6,7 @@ hmProjectInfo = {
 	copyright: "© 2024 National Renewable Energy Laboratory",
 	summary: "&nbsp;",
 	version: "0.0.1",
-	date: "Thursday, June 13, 2024",
+	date: "Tuesday, July 16, 2024",
 	language: "en-us",
 	mainfile: "index.html"
 	}
