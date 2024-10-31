110aa1ce14b8f61c5ae163b39e8db94f55b423de
Brian Mirletz <brian.mirletz@nrel.gov>
2024-10-31 15:01:15 -0600

    re-add variable to deal with conflicts from custom generation name change

--- samples/Python Scripts/End Use Load Profiles/load-data-example.py
+++ samples/Python Scripts/End Use Load Profiles/load-data-example.py
@@ -61,17 +61,17 @@ from eulp import get, plot, stats
 ## Set these parameters before running the script! ##
 
 # 1. Choose a U.S. state and county:
-state = 'CO'
-county_name = 'Jefferson'
+state = 'FL'
+county_name = 'Broward'
 
 # 2. Get these parameters from the data lake website:
-year = '2022'
-dataset = 'resstock_amy2018_release_1'
+year = '2021'
+dataset = 'comstock_amy2018_release_1'
 upgrade = 'baseline'
 
 # 3. Set the path where you want to store files
 # (files are large, you'll need at least 200 MB of space):
-path=''
+path='Sample Results'
 
 ## End Parameters ##
 
