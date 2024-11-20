69ddffc27f4aa614eebd15b0a76cab0ecff8682d
Paul Gilman <cpaulgilman@gmail.com>
2024-11-20 15:22:31 -0800

    Preliminary Help revisions for Beta build

--- deploy/runtime/help/html/js/hmXtopic.js
+++ deploy/runtime/help/html/js/hmXtopic.js
@@ -6,7 +6,7 @@ hmProjectInfo = {
 	copyright: "© 2024 National Renewable Energy Laboratory",
 	summary: "&nbsp;",
 	version: "0.0.1",
-	date: "Monday, October 28, 2024",
+	date: "Wednesday, November 20, 2024",
 	language: "en-us",
 	mainfile: "index.html"
 	}
