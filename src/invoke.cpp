e40e7cfe7ba4a3bfb3dce3f9a3e917f99a847cf4
Steven Janzou <steven@janzouconsulting.com>
2026-09-04 04:31:20 -0600

    Fix bug in fcall_var_exists for hybrids

--- src/invoke.cpp
+++ src/invoke.cpp
@@ -1950,12 +1950,13 @@ void fcall_var_exists(lk::invoke_t& cxt)
         c = SamApp::Window()->GetCurrentCase();
     if (c != nullptr) {
         wxString name = cxt.arg(0).as_string();
+		std::string strName = name.ToStdString();
         auto cfg = c->GetConfiguration();
         int ndxHybrid = 0;
         VarValue* vv = NULL;
         bool bfound = false;
         for (size_t ndx = 0; ndx < cfg->Technology.size(); ndx++) { // select ndxHybrid based on compute module position in
-            if (vv = c->Values(ndxHybrid).Get(name)) {
+            if (vv = c->Values(ndx).Get(name)) {
                 bfound = true;
                 ndxHybrid = ndx;
             }
